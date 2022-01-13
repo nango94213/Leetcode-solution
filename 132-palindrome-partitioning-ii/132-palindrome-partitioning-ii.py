56df83e4fe084ce473be83e4278abc5d603c55d8
Yinan Yang <845280629abc@gmail.com>
2022-01-12 23:53:18 -0600

    Time: 6337 ms (5.10%), Space: 14.2 MB (95.67%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.py
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.py
@@ -1,12 +1,13 @@
 class Solution:
     def minCut(self, s: str) -> int:
+        
+        
         dp = [float('inf')]*len(s)
         dp[0] = 0
         
         for i in range(1, len(s)):
             if s[:i+1] == s[:i+1][::-1]:
                 dp[i] = 0
-
             else:
                 for j in range(i):
                     if dp[j] != float('inf') and s[j+1:i+1] == s[j+1:i+1][::-1]:
