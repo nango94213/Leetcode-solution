ff997916a1cf9401cddf97f9520a200049bedfca
Yinan Yang <845280629abc@gmail.com>
2022-01-13 15:34:29 -0600

    Time: 471 ms (54.52%), Space: 22.1 MB (63.76%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,17 +1,17 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        dp_grid = [[0] * (len(text2) + 1) for _ in range(len(text1) + 1)]
+        dp = [[0] * (len(text2) + 1) for i in range(len(text1) + 1) ]
         
-        # Iterate up each column, starting from the last one.
-        for col in reversed(range(len(text2))):
-            for row in reversed(range(len(text1))):
-                # If the corresponding characters for this cell are the same...
-                if text2[col] == text1[row]:
-                    dp_grid[row][col] = 1 + dp_grid[row + 1][col + 1]
-                # Otherwise they must be different...
+        for i in reversed(range(len(text1))):
+            for j in reversed(range(len(text2))):
+                
+                if text1[i] == text2[j]:
+                    
+                    dp[i][j] = 1 + dp[i+1][j+1]
+                    
                 else:
-                    dp_grid[row][col] = max(dp_grid[row + 1][col], dp_grid[row][col + 1], dp_grid[row+1][col+1])
+                    
+                    dp[i][j] = max(dp[i+1][j], dp[i][j+1])
         
-        # The original problem's answer is in dp_grid[0][0]. Return it.
-        return dp_grid[0][0]
+        return dp[0][0]
         
