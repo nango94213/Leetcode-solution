755b52a4e744312383cbd4618f7c9789c7cad904
Yinan Yang <845280629abc@gmail.com>
2022-01-22 22:27:29 -0600

    Time: 1949 ms (32.87%), Space: 14.1 MB (91.93%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.py
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.py
@@ -4,8 +4,6 @@ class Solution:
         direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
         def dfs(i,j,path):
             nonlocal res
-            if grid[i][j]==0:
-                return
             
     
             res = max(path, res)
@@ -16,14 +14,14 @@ class Solution:
                 new_i = i + d[0]
                 new_j = j + d[1]
                 
-                if 0 <= new_i < len(grid) and 0<= new_j < len(grid[0]):
+                if 0 <= new_i < len(grid) and 0<= new_j < len(grid[0]) and grid[new_i][new_j]!=0:
                     dfs(new_i, new_j, path+grid[new_i][new_j])
             grid[i][j] = tmp
  
 
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j]!='0':
+                if grid[i][j]!=0:
                     dfs(i,j, grid[i][j])
                     
         
