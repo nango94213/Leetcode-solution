388927758a2fd6623d8767d38a4c149a9be8d912
Yinan Yang <845280629abc@gmail.com>
2022-01-22 22:23:57 -0600

    Time: 1647 ms (52.59%), Space: 14.3 MB (48.61%) - LeetHub

---
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.py
@@ -0,0 +1,33 @@
+class Solution:
+    def getMaximumGold(self, grid: List[List[int]]) -> int:
+        res = 0
+        direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        def dfs(i,j,path):
+            nonlocal res
+            if grid[i][j]==0:
+                return
+            
+    
+            res = max(path, res)
+            
+            tmp = grid[i][j]
+            grid[i][j]=0
+            for d in direction:
+                new_i = i + d[0]
+                new_j = j + d[1]
+                
+                if 0 <= new_i < len(grid) and 0<= new_j < len(grid[0]):
+                    dfs(new_i, new_j, path+grid[new_i][new_j])
+            grid[i][j] = tmp
+ 
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j]!='0':
+                    dfs(i,j, grid[i][j])
+                    
+        
+        
+        
+        
+        return res
