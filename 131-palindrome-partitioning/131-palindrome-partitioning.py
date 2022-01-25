2e7c9aebc385e906413064344b5019f4bb87d6d0
Yinan Yang <845280629abc@gmail.com>
2022-01-24 19:53:42 -0600

    Time: 1452 ms (5.02%), Space: 31.2 MB (21.65%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.py
+++ 131-palindrome-partitioning/131-palindrome-partitioning.py
@@ -1,26 +1,19 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
+        
         res = []
-        def ispad(part):
-            l, r = 0, len(part) - 1
-            while l < r:
-                
-                if part[l] != part[r]:
-                     return False
-                
-                l += 1
-                r -= 1
-            return True
-    
+        
         def dfs(pool, path):
+            
             if not pool:
                 res.append(path)
                 return
-
+                
             for i in range(len(pool)):
-                if pool[:i+1] == pool[:i+1][::-1] :
-                    dfs(pool[i+1:], path + [pool[:i+1]])
+                
+                if pool[:i+1] == pool[:i+1][::-1]:
+                    dfs(pool[i+1:], path+[pool[:i+1]])
         
         dfs(s, [])
-       
+        
         return res
