cd431c9330a1bb8724ff7180edc1bfa37446bd2f
Yinan Yang <845280629abc@gmail.com>
2022-01-28 21:34:24 -0600

    Time: 1067 ms (29.59%), Space: 20.4 MB (47.15%) - LeetHub

---
+++ 886-possible-bipartition/886-possible-bipartition.py
@@ -0,0 +1,28 @@
+import collections
+class Solution:
+    def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
+        graph = collections.defaultdict(set)
+        
+        for a, b in dislikes:
+            graph[a].add(b)
+            graph[b].add(a)
+        
+        seen = set()
+        two_group = {}
+        for i in range(1, n+1):
+            if i not in seen:
+                seen.add(i)
+                stack = collections.deque([(i, True)])
+            
+                while stack:
+                    person, group = stack.popleft()
+                    two_group[person] = group
+                    for child in graph[person]:
+                            if child in two_group and (two_group[child] != (not group)):
+                                    return False
+                            if child not in seen:
+                                seen.add(child)
+                                stack.append((child, not group))
+        return True
+                
+                
