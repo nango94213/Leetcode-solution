98bfcea3bfb8ecb126178f7e38f65dffa14cc1e3
Yinan Yang <845280629abc@gmail.com>
2022-01-29 16:46:59 -0600

    Time: 1256 ms (66.11%), Space: 14.2 MB (37.40%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,163 +1,17 @@
 class Solution(object):
     def coinChange(self, coins, amount):
-        '''dic=[float('inf')]*(amount+1)
-        dic[0]=0
-        for i in range(1,len(dic)):
-            for j in coins:
-                if amount-j>=0:
-                    dic[i]=min(dic[i],dic[i-j]+1)
-        return dic[amount] if dic[amount]!=float('inf') else -1
-        '''
-        
-        dic=[float('inf')]*(amount+1)
-        dic[0]=0
-        for i in coins:
-            for j in range(i,amount+1):
-                dic[j]=min(dic[j],dic[j-i]+1)
-        return dic[amount] if dic[amount]!=float('inf') else -1
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
         """
         :type coins: List[int]
         :type amount: int
         :rtype: int
-        
-       
-       
-       
-        dp=[-1 for i in range(amount+1)]
-        dp[0]=0
-        for i in range(1,len(dp)):
-            if i in coins:
-                dp[i]=1
-            else:
-                minn=amount+1
-                for j in coins:
-                    if i-j>=0:
-                        if dp[i-j]!=-1:
-                            minn=min(minn,dp[i-j]+1)
-                if minn!=amount+1:
-                    dp[i]=minn
-        return dp[amount]    
-       
         """
+        dp = [float('inf')] * (amount+1)
         
+        dp[0] = 0
         
-       
+        for i in range(1, amount+1):
+            for j in coins:
+                if (i - j) >= 0:
+                    dp[i] = min(dp[i], dp[i-j] + 1)
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        """if amount==0:
-            return 0
-        
-        dp=[-1 for i in range(amount)]
-        
-        
-        for i in range(1,amount+1):
-            if i in coins:
-                dp[i-1]=1
-            else:
-                for j in coins:
-                    if j<i and dp[i-j-1]!=-1:
-                        if dp[i-1]==-1:
-                            dp[i-1]=amount
-                        dp[i-1]=min(dp[i-1],dp[i-j-1]+1)
-                            
-        return dp[amount-1]"""
+        return dp[amount] if dp[amount] != float('inf') else -1
