cf490a9a5362e87c1988b64ad6f1c60a9cc5339e
Yinan Yang <845280629abc@gmail.com>
2022-01-28 21:57:24 -0600

    Time: 922 ms (39.59%), Space: 20.7 MB (43.98%) - LeetHub

--- 886-possible-bipartition/886-possible-bipartition.py
+++ 886-possible-bipartition/886-possible-bipartition.py
@@ -1,31 +1,28 @@
 import collections
 class Solution:
-    def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
-        graph = collections.defaultdict(set)
+    def possibleBipartition(self, N: int, dislikes: List[List[int]]) -> bool:
+        self.graph = collections.defaultdict(list)
+        group_mapping = {}
+        for (u, v) in dislikes:                             # Create graph
+            self.graph[u].append(v)
+            self.graph[v].append(u)
         
-        for a, b in dislikes:
-            graph[a].add(b)
-            graph[b].add(a)
-        
-        seen = set()
-        two_group = {}
-        for i in range(1, n+1):
-            if i not in seen:
-                stack = collections.deque([(i, True)])
-            
-                while stack:
-                    person, group = stack.popleft()
-                    if person not in two_group:
-                         two_group[person] = group
-                    seen.add(person)
-                    for child in graph[person]:
-                            if child in seen:
-                                continue
-                            if child in two_group and (two_group[child] != (not group)):
-                                    return False
-                            stack.append((child, not group))
-                            two_group[child] = not group
-                    
-        return True
+        visited = set()
+        for i in range(1, N + 1):                           # Iterate each node
+            if i in visited: continue                       # No need to revisit, since it's a non-directed graph
+            stack = [(i, 0)]                                # Use stack for BFS
+            while stack:                                    # You can also use a deque instead of 2 while loop on stack
+                tmp_stack = []
+                while stack:                                # exhaust current stack before go to next layer (BFS)
+                    cur_node, group = stack.pop()
+                    if cur_node in group_mapping and group != group_mapping[cur_node]:  # check if it's conflict
+                        return False
+                    if cur_node in visited: continue        # If visited and no conflict, continue to avoid dead loop
+                    group_mapping[cur_node] = group         # Assign group for current node
+                    visited.add(cur_node)
+                    for child in self.graph[cur_node]:      # Assign contrary group for dislikes of current node
+                        tmp_stack.append((child, not group))
+                stack = tmp_stack            
+        return True  
                 
                 
