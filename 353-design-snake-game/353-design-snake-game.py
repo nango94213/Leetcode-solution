88d8485baa73a4421a9c79e143387578d61ca9d6
Yinan Yang <845280629abc@gmail.com>
2022-01-31 16:53:09 -0600

    Time: 248 ms (90.91%), Space: 15.7 MB (93.15%) - LeetHub

---
+++ 353-design-snake-game/353-design-snake-game.py
@@ -0,0 +1,40 @@
+import collections
+class SnakeGame:
+
+    def __init__(self, width: int, height: int, food: List[List[int]]):
+        
+        self.snake = collections.deque([(0, 0)])
+        self.snake_set = set([(0, 0)])
+        self.food_index = 0
+        self.food = food
+        self.width = width
+        self.height = height
+        self.d = {'L': (0, -1), 'U': (-1, 0), 'R': (0, 1), 'D': (1, 0)}
+
+    def move(self, direction: str) -> int:
+        
+        newHead = (self.snake[0][0] + self.d[direction][0], self.snake[0][1]+self.d[direction][1])
+
+        if newHead[0] < 0 or newHead[0] >= self.height or newHead[1] < 0 or newHead[1] >=self.width:
+            return -1
+        
+        if  newHead != self.snake[-1] and newHead in self.snake_set:
+            return -1
+        
+        current_food = self.food[self.food_index] if self.food_index < len(self.food) else None
+        
+        if current_food and (current_food[0] == newHead[0]) and (current_food[1] == newHead[1]):
+            self.food_index += 1
+        else:
+            tail = self.snake.pop()
+            self.snake_set.remove(tail)
+        
+        self.snake.appendleft(newHead)
+        self.snake_set.add(newHead)
+        
+        return len(self.snake) - 1
+
+
+# Your SnakeGame object will be instantiated and called as such:
+# obj = SnakeGame(width, height, food)
+# param_1 = obj.move(direction)
