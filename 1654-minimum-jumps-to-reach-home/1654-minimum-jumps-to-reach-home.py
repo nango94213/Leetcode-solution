d62e9eaca5a05385e60f7fdd041ab2d877f108b3
Yinan Yang <845280629abc@gmail.com>
2022-02-01 23:26:08 -0600

    Time: 207 ms (23.84%), Space: 15.1 MB (44.91%) - LeetHub

--- 1654-minimum-jumps-to-reach-home/1654-minimum-jumps-to-reach-home.py
+++ 1654-minimum-jumps-to-reach-home/1654-minimum-jumps-to-reach-home.py
@@ -4,35 +4,37 @@ class Solution:
         
         limit = 2000 + a + b
         
-        q = deque([(0, True)])
-        seen = {(0, True)}
+        q = collections.deque([(0, True)])
+        seen = set([(0, True)])
         
         for bad in forbidden:
             seen.add((bad, True))
             seen.add((bad, False))
         
         steps = 0
-        
         while q:
+            
             for _ in range(len(q)):
-                current, isforward = q.popleft()
+                current, is_forward = q.popleft()
                 
                 if current == x:
                     return steps
-                forward, backward = (current+a, True), (current-b, False)
-                if (current + a) <=limit and forward not in seen:
-                    q.append(forward)
-                    seen.add(forward)
                 
-                if isforward and (current-b) > 0 and backward not in seen:
-                    q.append(backward)
-                    seen.add(backward)
-            steps += 1
-        return -1
-
-
+                next_jump = current + a
                 
+                if next_jump <= limit and (next_jump, True) not in seen:
+                    q.append((next_jump, True))
+                    seen.add((next_jump, True))
+                
+                if is_forward:
+                    next_jump = current - b
+                    if next_jump > 0 and (next_jump, False) not in seen:
+                        q.append((next_jump, False))
+                        seen.add((next_jump, False))
             
+            steps += 1
+        
+        return -1
                         
                 
                 
