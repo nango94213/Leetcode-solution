64e76a4e59a3b1f61b6617fd5f9a7e942d19a15b
Yinan Yang <845280629abc@gmail.com>
2022-02-02 18:12:53 -0600

    Time: 277 ms (39.28%), Space: 19.8 MB (29.14%) - LeetHub

---
+++ 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.py
@@ -0,0 +1,61 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Codec:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        res = []
+        
+        def to_string(node):
+            if node:
+                res.append(str(node.val)+',')
+                to_string(node.left)
+                to_string(node.right)
+            else:
+                res.append('None,')
+        
+        to_string(root)
+        
+        return ''.join(res)
+        
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        
+        def to_tree():
+            if l[0] == 'None':
+                l.pop(0)
+                return
+            
+            root = TreeNode(l[0])
+            l.pop(0)
+            
+            root.left = to_tree()
+            root.right = to_tree()
+            
+            return root
+        
+        l = data.split(',')
+        
+        return to_tree()
+                
+        
+        
+
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# ans = deser.deserialize(ser.serialize(root))
