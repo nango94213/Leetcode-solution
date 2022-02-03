88c689aa07aeb229d7a8df7e50426284adbe936b
Yinan Yang <845280629abc@gmail.com>
2022-02-03 15:14:11 -0600

    Time: 6168 ms (5.07%), Space: 14 MB (98.38%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.py
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.py
@@ -1,8 +1,8 @@
 class Solution:
     def minCut(self, s: str) -> int:
         
+        dp = [float('inf')] * len(s)
         
-        dp = [float('inf')]*len(s)
         dp[0] = 0
         
         for i in range(1, len(s)):
@@ -11,8 +11,6 @@ class Solution:
             else:
                 for j in range(i):
                     if dp[j] != float('inf') and s[j+1:i+1] == s[j+1:i+1][::-1]:
-                        dp[i] = min(dp[i], dp[j] + 1)
-        return  dp[-1]
-                    
-            
+                        dp[i] = min(dp[i], dp[j]+1)
         
+        return dp[-1]
