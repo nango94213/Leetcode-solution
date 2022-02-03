8c42f11880d8d039b65abffc5f031ed1be4da297
Yinan Yang <845280629abc@gmail.com>
2022-02-03 13:38:15 -0600

    Time: 1916 ms (6.96%), Space: 29 MB (49.08%) - LeetHub

---
+++ 1345-jump-game-iv/1345-jump-game-iv.py
@@ -0,0 +1,51 @@
+import collections
+class Solution:
+    def minJumps(self, arr: List[int]) -> int:
+        
+        dic = collections.defaultdict(list)
+        
+        for i, v in enumerate(arr):
+            dic[v].append(i)
+
+        q = collections.deque([0])
+        
+        seen = set([0])
+        
+        jumps = 0
+        
+        goal = len(arr) - 1
+        
+        while q:
+            print(q)
+            for _ in range(len(q)):
+                
+                current = q.popleft()
+                
+                if current == goal:
+                    return jumps
+                
+                next_left = current - 1
+                next_right = current + 1
+                
+                sames = dic[arr[current]]
+
+                if (0 <= next_left <= goal) and (next_left not in seen):
+                    seen.add(next_left)
+                    q.append(next_left)
+                
+                if (0 <= next_right <= goal) and (next_right not in seen):
+                    seen.add(next_right)
+                    q.append(next_right)
+                
+                for same in sames:
+                    if same not in seen:
+                        seen.add(same)
+                        q.append(same)
+                del dic[arr[current]]
+            
+            jumps += 1
+        
+        return jumps
+                    
+                    
+        
