3268376895ede26c4ef80fd320ef7c31e8e7c9a4
Yinan Yang <845280629abc@gmail.com>
2022-02-17 00:36:32 -0600

    Time: 7395 ms (5.06%), Space: 19.3 MB (51.88%) - LeetHub

---
+++ 403-frog-jump/403-frog-jump.py
@@ -0,0 +1,31 @@
+class Solution:
+    def canCross(self, stones: List[int]) -> bool:
+        if stones[1]!=1:
+            return False
+            
+        
+        stack = [(1, 1)]
+        
+        seen = set([(1, 1)])
+        target = stones[-1]
+        stones_set = set(stones)
+        while stack:
+            for _ in range(len(stack)):
+                k, p = stack.pop(0)
+                if p == target:
+                    return True
+            
+                if (p+k) in stones and 0<=k<=target and (k, p+k) not in seen:
+                    seen.add((k, p+k))
+                    stack.append((k, p+k))
+                
+                if (p+k+1) in stones and 0<=k+1<=target and (k+1, p+k+1) not in seen:
+                    seen.add((k+1, p+k+1))
+                    stack.append((k+1, p+k+1))
+                
+                if (p+k-1) in stones and 0<=k-1<=target and (k-1, p+k-1) not in seen:
+                    seen.add((k-1, p+k-1))
+                    stack.append((k-1, p+k-1))
+        
+        return False
+        
