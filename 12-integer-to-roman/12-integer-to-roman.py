0114932ce26c547953f48381e38557b4492366e5
Yinan Yang <845280629abc@gmail.com>
2022-03-08 21:51:35 -0600

    Time: 74 ms (48.63%), Space: 13.8 MB (99.37%) - LeetHub

---
+++ 12-integer-to-roman/12-integer-to-roman.py
@@ -0,0 +1,8 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        roman={1000: 'M', 900: 'CM', 500: 'D', 400: 'CD', 100: 'C', 90: 'XC', 50: 'L', 40: 'XL', 10: 'X', 9: 'IX', 5: 'V', 4: 'IV', 1: 'I'} 
+        ans=''
+        for k,v in roman.items():
+            ans += (num//k*v)
+            num%=k
+        return ans
