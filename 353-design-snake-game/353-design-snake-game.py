6340712c27486d6d30c2e718d79de8412aabdcc5
Yinan Yang <845280629abc@gmail.com>
2022-03-14 17:44:57 -0500

    Time: 294 ms (69.14%), Space: 16 MB (14.93%) - LeetHub

--- 353-design-snake-game/353-design-snake-game.py
+++ 353-design-snake-game/353-design-snake-game.py
@@ -13,12 +13,12 @@ class SnakeGame:
 
     def move(self, direction: str) -> int:
         
-        newHead = (self.snake[0][0] + self.d[direction][0], self.snake[0][1]+self.d[direction][1])
+        newHead = (self.snake[-1][0] + self.d[direction][0], self.snake[-1][1]+self.d[direction][1])
 
         if newHead[0] < 0 or newHead[0] >= self.height or newHead[1] < 0 or newHead[1] >=self.width:
             return -1
         
-        if  newHead != self.snake[-1] and newHead in self.snake_set:
+        if  newHead != self.snake[0] and newHead in self.snake_set:
             return -1
         
         current_food = self.food[self.food_index] if self.food_index < len(self.food) else None
@@ -26,10 +26,10 @@ class SnakeGame:
         if current_food and (current_food[0] == newHead[0]) and (current_food[1] == newHead[1]):
             self.food_index += 1
         else:
-            tail = self.snake.pop()
+            tail = self.snake.popleft()
             self.snake_set.remove(tail)
         
-        self.snake.appendleft(newHead)
+        self.snake.append(newHead)
         self.snake_set.add(newHead)
         
         return len(self.snake) - 1
