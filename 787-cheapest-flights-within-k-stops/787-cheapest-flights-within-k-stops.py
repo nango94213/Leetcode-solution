11385ecf83169554d5e0f0bbc7a510c1aab899f1
Yinan Yang <845280629abc@gmail.com>
2022-03-14 22:56:12 -0500

    Time: 975 ms (5.00%), Space: 15.7 MB (16.89%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.py
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.py
@@ -26,7 +26,7 @@ class Solution:
                 if current == dst:
 
                     res = min(res, cost[(current, stops)])
-                    continue
+                    #continue
                 
                 for outgoing in dic[current].keys():
       
