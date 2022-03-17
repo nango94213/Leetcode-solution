3ff5ac83b195582bbc660054722bca48c190c38a
Yinan Yang <845280629abc@gmail.com>
2022-03-16 19:05:39 -0500

    Time: 157 ms (29.04%), Space: 15.8 MB (82.33%) - LeetHub

---
+++ 759-employee-free-time/759-employee-free-time.py
@@ -0,0 +1,34 @@
+"""
+# Definition for an Interval.
+class Interval:
+    def __init__(self, start: int = None, end: int = None):
+        self.start = start
+        self.end = end
+"""
+class Solution:
+    def employeeFreeTime(self, schedule: '[[Interval]]') -> '[Interval]':
+
+        h = heapq.merge(*schedule, key=lambda x: x.start)
+        h = list(h)
+        res = []
+        current = h[0]
+        
+        b = current.end
+        for i in h[1:]:
+            if b < i.start:
+                tmp = Interval()
+                tmp.start = b
+                tmp.end = i.start
+                res.append(tmp)
+                b = i.end
+            else:
+                b = max(b, i.end)
+        
+        return res
+            
+            
+
+            
+
+        
+            
