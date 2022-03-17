de13c1cf58eee39056caeb4d9dc348ed386b68b7
Yinan Yang <845280629abc@gmail.com>
2022-03-17 17:17:31 -0500

    Time: 180 ms (96.30%), Space: 16.9 MB (50.79%) - LeetHub

---
+++ 529-minesweeper/529-minesweeper.py
@@ -0,0 +1,42 @@
+class Solution:
+    def updateBoard(self, board: List[List[str]], click: List[int]) -> List[List[str]]:
+        direction=[(-1,-1),(-1,0),(-1,1),(0,-1),(0,1),(1,-1),(1,0),(1,1)]
+        row_n=len(board)
+        col_n=len(board[0])
+        
+        def dfs(i,j):
+            if board[i][j]!='E':
+                return
+            
+            count=0
+            
+            for d in direction:
+                x=i+d[0]
+                y=j+d[1]
+                
+                if 0<=x<row_n and 0<=y<col_n and board[x][y]=='M':
+                    count+=1
+            
+            if count:
+                board[i][j]=str(count)
+                return
+            
+            board[i][j]='B'
+            
+            for d in direction:
+                x=i+d[0]
+                y=j+d[1]
+                if 0<=x<row_n and 0<=y<col_n:
+                     dfs(x,y)
+                        
+        if board[click[0]][click[1]]=='M':
+            board[click[0]][click[1]]='X'
+            return board
+        
+        dfs(click[0],click[1])
+        
+        return board
+                
+                
+                
+        
