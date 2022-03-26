0e5501cf0063fd93bd8b534d0324667803423030
Yinan Yang <845280629abc@gmail.com>
2022-03-26 17:19:06 -0500

    Time: 65 ms (36.96%), Space: 13.9 MB (83.60%) - LeetHub

---
+++ 377-combination-sum-iv/377-combination-sum-iv.py
@@ -0,0 +1,15 @@
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        
+        dp = [0 for i in range(target+1)]
+        dp[0] = 1
+
+        for comb_sum in range(target+1):
+
+            for num in nums:
+                if comb_sum - num >= 0:
+                    dp[comb_sum] += dp[comb_sum-num]
+                # minor optimization, early stopping.
+                # else:
+                #    break
+        return dp[target]
