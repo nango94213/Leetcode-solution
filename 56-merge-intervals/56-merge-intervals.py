9b52757958abdd06bb7c7142c32b2ebfbc7e5bd4
Yinan Yang <845280629abc@gmail.com>
2022-04-01 22:40:52 -0500

    Time: 336 ms (5.02%), Space: 18.1 MB (52.76%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.py
+++ 56-merge-intervals/56-merge-intervals.py
@@ -3,12 +3,15 @@ class Solution:
         
         intervals.sort()
         
-        stack = [intervals[0]]
+        stack = []
         
-        for i in intervals[1:]:
+        for i in intervals:
             
-            if stack[-1][1] < i[0]:
-                stack.append(i)
-            else:
+            if stack and stack[-1][1] >= i[0]:
                 stack[-1][1] = max(stack[-1][1], i[1])
+            
+            else:
+                stack.append(i)
+        
         return stack
+        
