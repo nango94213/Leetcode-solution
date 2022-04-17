46669127e5827d31e9a63f20bb4636e3b21d4285
Yinan Yang <845280629abc@gmail.com>
2022-04-17 16:35:22 -0500

    Time: 175 ms (57.64%), Space: 13.8 MB (99.64%) - LeetHub

---
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
@@ -0,0 +1,21 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+
+        left = 0
+        longest = 0
+        dic = collections.defaultdict(int)
+        
+        for right in range(len(s)):
+            
+            dic[s[right]] +=  1
+            
+            while (right-left+1)-max(dic.values()) > k:
+                
+                dic[s[left]] -= 1
+                
+                left += 1
+                
+            longest = max(longest, right-left+1)
+            
+        
+        return longest
