24b39b205255248a6fab9989d212b04a7d599b94
Yinan Yang <845280629abc@gmail.com>
2022-04-17 16:50:22 -0500

    Time: 134 ms (63.89%), Space: 14.7 MB (84.84%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.py
+++ 76-minimum-window-substring/76-minimum-window-substring.py
@@ -1,31 +1,42 @@
-import collections
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         
-        target = collections.Counter(t)
+        
         found = 0
-        l = 0
+        
+        count = Counter(t)
+        
+        left = 0
+        
         res = ''
-
-        for r in range(len(s)):
-            if s[r] in target:
+        
+        for right in range(len(s)):
+            
+            if s[right] in count:
                 
-                target[s[r]] -= 1
-                if target[s[r]] >= 0:
+                count[s[right]] -= 1
+                
+                if count[s[right]] >= 0:
+                    
                     found += 1
-
-
-            while found==len(t):
-                if not res or (r-l+1) < len(res):
-                    res = s[l:r+1]
+            
+            while found == len(t):
                 
-                if s[l] in target:
-                    target[s[l]] += 1
-                    if target[s[l]] > 0: 
+                if not res or (right-left+1) < len(res):
+                    
+                    res = s[left:right+1]
+                
+                if s[left] in count:
+                    
+                    count[s[left]] += 1
+                    
+                    if count[s[left]] > 0:
                         found -= 1
-                l += 1
+                
+                left += 1
+        
         
         return res
-        
-        
-        
+                    
+            
+            
