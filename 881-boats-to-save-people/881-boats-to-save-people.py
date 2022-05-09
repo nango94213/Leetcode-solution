e915b3e6b86c28d228d492e70df5f473e850a7c9
Yinan Yang <845280629abc@gmail.com>
2022-05-09 02:33:33 -0500

    Time: 481 ms (84.85%), Space: 21 MB (30.14%) - LeetHub

--- 881-boats-to-save-people/881-boats-to-save-people.py
+++ 881-boats-to-save-people/881-boats-to-save-people.py
@@ -15,11 +15,8 @@ class Solution:
             if people[left] + people[right] <= limit:
                 
                 left += 1
-                right -= 1
-            
-            else:
-                
-                right -= 1
+             
+            right -= 1
             
             count += 1
         
