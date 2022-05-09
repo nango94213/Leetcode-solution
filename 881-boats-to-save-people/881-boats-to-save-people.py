b44047a37bf1dc56f5e4044fc9f04ad99b26495c
Yinan Yang <845280629abc@gmail.com>
2022-05-09 02:32:57 -0500

    Time: 466 ms (89.41%), Space: 20.8 MB (75.48%) - LeetHub

---
+++ 881-boats-to-save-people/881-boats-to-save-people.py
@@ -0,0 +1,28 @@
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        
+
+        
+        left = 0
+        right = len(people) - 1
+        
+        people.sort()
+        
+        count = 0
+        
+        while left <= right:
+            
+            if people[left] + people[right] <= limit:
+                
+                left += 1
+                right -= 1
+            
+            else:
+                
+                right -= 1
+            
+            count += 1
+        
+        return count
+                
+        
