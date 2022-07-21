9c0eb3c97c73f5d687af13f242fa8d72e87d31bc
Yinan Yang <845280629abc@gmail.com>
2022-07-21 12:17:06 -0500

    Time: 631 ms (23.25%), Space: 0B (100.00%) - LeetHub

---
+++ 603-consecutive-available-seats/603-consecutive-available-seats.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+
+
+select distinct a.seat_id
+from cinema a cross join cinema b
+  on abs(a.seat_id - b.seat_id) = 1
+  and a.free = true and b.free = true
+order by a.seat_id
+;
