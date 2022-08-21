50ec7f1ce16cb33e12a3711a93c96aec04b1ea78
Yinan Yang <845280629abc@gmail.com>
2022-08-20 19:36:33 -0500

    Time: 257 ms (61.83%), Space: 14.2 MB (19.36%) - LeetHub

--- 723-candy-crush/723-candy-crush.py
+++ 723-candy-crush/723-candy-crush.py
@@ -4,28 +4,25 @@ class Solution:
         m = len(board)
         n = len(board[0])
         
-    
+        
+        
         
         while True:
             remove = set()
+            for j in range(n):
+                for i in range(m-2):
+                    if board[i][j] == board[i+1][j] == board[i+2][j] != 0:
+                        remove.update([(i, j), (i+1, j), (i+2, j)])
             
             
             for i in range(m):
                 for j in range(n-2):
-                
                     if board[i][j] == board[i][j+1] == board[i][j+2] != 0:
                         remove.update([(i, j), (i, j+1), (i, j+2)])
             
             
-            for j in range(n):
-                for i in range(m-2):
-                    
-                    if board[i][j] == board[i+1][j] == board[i+2][j] != 0:
-                        remove.update([(i, j), (i+1, j), (i+2, j)])
-            
             if not remove:
                 return board
-            
             
             
             for j in range(n):
@@ -33,10 +30,10 @@ class Solution:
                 for i in range(m-1, -1, -1):
                     if (i, j) in remove:
                         falling += 1
-                    else:
-                        board[i+falling][j] = board[i][j]
-                        
+                        continue
+                    
+                    board[i+falling][j] = board[i][j]
+                
                 for i in range(falling):
                     board[i][j] = 0
-            
-            
+                    
