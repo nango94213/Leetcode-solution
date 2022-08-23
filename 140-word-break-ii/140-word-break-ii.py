cdc57d1a0560f34af7ebc5e1284a6ae3411ae5e1
Yinan Yang <845280629abc@gmail.com>
2022-08-23 18:50:09 -0500

    Time: 58 ms (20.10%), Space: 13.9 MB (33.18%) - LeetHub

--- 140-word-break-ii/140-word-break-ii.py
+++ 140-word-break-ii/140-word-break-ii.py
@@ -1,39 +1,26 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
-        wordDict = set(wordDict)
-        def check(g):
-            dp = [False] * len(g)
         
-            for i in range(len(g)):
-                if g[:i+1] in wordDict:
-                    dp[i] = True
-                else:
-                    for j in range(i):   # s[:i+1] = 'abcd'
-                        if dp[j] and g[j+1:i+1] in wordDict:
-                            dp[i] = True
-                            break
-                            
-            return dp[-1]
+        wordDict = set(wordDict)
+        
+        
+        
         
         res = []
         
         def dfs(pool, path):
             
-      
+            if not pool:
+                res.append(path[1:])
+            
+            for i in range(len(pool)):
                 
-                if not pool:
-                    res.append(path[1:])
-                    return
-                
-                if not check(pool):
-                    return
-                
-                for i in range(len(pool)):
+                if pool[:i+1] in wordDict:
                     
-                    if pool[:i+1] in wordDict:
-                        
-                        dfs(pool[i+1:], path+" "+pool[:i+1])
-        dfs(s, '')
+                    dfs(pool[i+1:], path+" "+pool[:i+1])
+        
+        dfs(s, "")
+        
         return res
                 
-        
+                
