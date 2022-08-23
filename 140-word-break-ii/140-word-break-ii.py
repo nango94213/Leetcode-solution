da665339373a44dfdea9e4d2a17eeb5fd8da3f9d
Yinan Yang <845280629abc@gmail.com>
2022-08-23 17:49:30 -0500

    Time: 47 ms (50.56%), Space: 13.8 MB (82.08%) - LeetHub

--- 140-word-break-ii/140-word-break-ii.py
+++ 140-word-break-ii/140-word-break-ii.py
@@ -12,17 +12,21 @@ class Solution:
                         if dp[j] and g[j+1:i+1] in wordDict:
                             dp[i] = True
                             break
-
+                            
             return dp[-1]
         
         res = []
         
         def dfs(pool, path):
             
-            if not pool or check(pool):
+      
                 
                 if not pool:
                     res.append(path[1:])
+                    return
+                
+                if not check(pool):
+                    return
                 
                 for i in range(len(pool)):
                     
