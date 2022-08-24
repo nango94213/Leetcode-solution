464a92e11b930cec8ba493ec55e84a13a9ed5e74
Yinan Yang <845280629abc@gmail.com>
2022-08-24 00:15:53 -0500

    Time: 364 ms (69.07%), Space: 28.9 MB (51.43%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,110 +1,28 @@
 class Solution(object):
     def numIslands(self, grid):
-        direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-        def dfs(i,j):
-    
-            grid[i][j]='0'
-            for d in direction:
-                new_i = i + d[0]
-                new_j = j + d[1]
-                
-                if 0 <= new_i < len(grid) and 0<= new_j < len(grid[0]) and grid[new_i][new_j] == '1':
-                    dfs(new_i, new_j)
-        count=0
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j]=='1':
-                    count+=1
-                    dfs(i,j)
-                    
-        
-        
-        
-        
-        return count
-            
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
         """
         :type grid: List[List[str]]
         :rtype: int
-        
-        class Solution(object):
-    def numIslands(self, grid):
-     
-        if grid==None or len(grid)==0:
-            return 0
-        grid=grid
-        self.r=len(grid)
-        self.c=len(grid[0])
-        count=0
-        for i in range(self.r):
-            for j in range(self.c):
-                if grid[i][j]=='1':
-                    count+=1
-                    self.dfs(i,j,grid)
-        return count
-                    
-    def dfs(self,i,j,grid):
-        if i<0 or j<0  or i>=self.r or j>=self.c or grid[i][j]=='0':
-            return
-        grid[i][j]='0'
-        self.dfs(i-1,j,grid)
-        self.dfs(i+1,j,grid)
-        self.dfs(i,j-1,grid)
-        self.dfs(i,j+1,grid)
         """
+        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        res = 0
         
+        def dfs(x, y):
+            
+            grid[x][y] = '0'
+            
+            for d in directions:
+                newX = x + d[0]
+                newY = y + d[1]
+                
+                if 0 <= newX <len(grid) and 0 <= newY < len(grid[0]) and grid[newX][newY] == '1':
+                    dfs(newX, newY)
+        
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                
+                if grid[i][j] == '1':
+                    res += 1
+                    dfs(i, j)
+        
+        return res
