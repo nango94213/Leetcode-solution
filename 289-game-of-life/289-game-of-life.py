c55cacc64596eeb2eab347238e67fd8a8f12d61d
Yinan Yang <845280629abc@gmail.com>
2022-08-24 15:05:57 -0500

    Time: 47 ms (63.36%), Space: 14 MB (48.70%) - LeetHub

---
+++ 289-game-of-life/289-game-of-life.py
@@ -0,0 +1,32 @@
+class Solution:
+    def gameOfLife(self, board: List[List[int]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        direction=[(-1,-1),(-1,0),(-1,1),(0,-1),(0,1),(1,-1),(1,0),(1,1)]
+        
+        copyBoard = copy.deepcopy(board)
+        
+        m, n = len(board), len(board[0])
+        
+        
+        for i in range(m):
+            for j in range(n):
+                
+                count = 0
+                
+                for d in direction:
+                    
+                    x = i + d[0]
+                    y = j + d[1]
+                    
+                    if 0 <= x < m and 0 <= y < n and copyBoard[x][y] == 1:
+                        count += 1
+      
+                if copyBoard[i][j] == 1 and (count < 2 or count >3):
+                    board[i][j] = 0
+                
+                if copyBoard[i][j] == 0 and count == 3:
+                    board[i][j] = 1
+       
+                
