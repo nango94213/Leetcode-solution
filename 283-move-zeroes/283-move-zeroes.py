5513a3c8aa87d3607a2354b7e2b4e62a984e77be
Yinan Yang <845280629abc@gmail.com>
2022-08-26 17:56:51 -0500

    Time: 391 ms (21.35%), Space: 15.6 MB (66.26%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.py
+++ 283-move-zeroes/283-move-zeroes.py
@@ -4,13 +4,11 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         
-        [1, 3, 12, 0, 0]
-        
         index = 0
         
         for i in range(len(nums)):
+            
             if nums[i] != 0:
-                nums[i], nums[index] = nums[index], nums[i]
+                nums[i-index], nums[i] = nums[i], nums[i-index]
+            else:
                 index += 1
-                
-        
