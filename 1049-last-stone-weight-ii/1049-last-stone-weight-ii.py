7ec1a41bd9468a86a92f3534cf38c90d872a3fb3
Yinan Yang <845280629abc@gmail.com>
2022-08-27 18:12:28 -0500

    Time: 193 ms (9.58%), Space: 14.7 MB (21.21%) - LeetHub

--- 1049-last-stone-weight-ii/1049-last-stone-weight-ii.py
+++ 1049-last-stone-weight-ii/1049-last-stone-weight-ii.py
@@ -1,24 +1,29 @@
 class Solution:
     def lastStoneWeightII(self, stones: List[int]) -> int:
- 
         s = sum(stones)
         
-        dp = [0] * (s+1)
-        dp[0] = 1
+        dp = [[0] * (s+1) for _ in range(len(stones)+1)]
         
-        for stone in stones:
-            for i in range(s, -1, -1):
+        for i in range(len(stones)+1):
+            dp[i][0] = 1
+        
+
+        for i in range(1, s+1):
+            for j in range(1, len(stones)+1):
                 
-                if dp[i] == 0 and i - stone >= 0 and dp[i-stone]:
-                    dp[i] = 1
-            
+                if i - stones[j-1] >= 0:
+                    if dp[j-1][i] or dp[j-1][i-stones[j-1]]:
+                        dp[j][i] = 1
+                else:
+                    dp[j][i] = dp[j-1][i]
+
         
         res = s
-        print(dp)
-        for psm in range(s+1):
-         
-            if dp[psm] and (2*psm-s) >= 0:
-               
+        
+        for psm in range(1, s+1):
+            
+            if dp[len(stones)][psm] and 2*psm - s >= 0:
                 res = min(res, 2*psm-s)
         
         return res
+   
