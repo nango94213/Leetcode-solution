abfe438684c13efa855f4782f4dc0713b6d058ff
Yinan Yang <845280629abc@gmail.com>
2022-08-28 19:23:35 -0500

    Time: 106 ms (88.56%), Space: 16 MB (7.58%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.py
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.py
@@ -2,8 +2,10 @@ import collections
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
         
-        q = collections.deque([(0, [0])])
+        q = collections.deque([[0, [0]]])
+        
         target = len(graph) - 1
+        
         res = []
         
         while q:
@@ -11,10 +13,12 @@ class Solution:
             current, path = q.popleft()
             
             if current == target:
-                res.append(path)
                 
-            for nxt in graph[current]:
-                q.append((nxt, path+[nxt]))
+                res.append(path)
+            
+            for n in graph[current]:
+                
+                q.append([n, path+[n]])
         
         return res
         
