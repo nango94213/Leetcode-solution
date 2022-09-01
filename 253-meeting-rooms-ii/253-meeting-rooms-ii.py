f69723518c7c82386c944114c785ec862438657b
Yinan Yang <845280629abc@gmail.com>
2022-09-01 01:52:11 -0500

    Time: 93 ms (81.63%), Space: 17.5 MB (83.80%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.py
@@ -4,9 +4,9 @@ class Solution:
         
         intervals.sort()
         
-        meeting = [intervals[0][1]]
+        meeting = []
         
-        for i in intervals[1:]:
+        for i in intervals:
             if meeting and meeting[0] <= i[0]:
                 heapq.heappop(meeting)
             
