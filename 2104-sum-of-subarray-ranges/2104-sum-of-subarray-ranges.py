60c58750d45f28c3f7e0694599ab6dc32a368633
Yinan Yang <845280629abc@gmail.com>
2022-09-05 18:35:03 -0500

    Time: 89 ms (98.77%), Space: 14.2 MB (11.09%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.py
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.py
@@ -1,7 +1,7 @@
 class Solution:
     def subArrayRanges(self, nums: List[int]) -> int:
         
-        res = 0
+        '''res = 0
         
         for i in range(len(nums)):
             l = nums[i]
@@ -14,5 +14,32 @@ class Solution:
                 
                 res += (r-l)
         
+        return res'''
+        
+        stack = []
+        stack2 = []
+        
+        arr = [float('-inf')] + nums + [float('-inf')]
+        arr2 = [float('inf')] + nums + [float('inf')]
+        
+        res = 0
+        
+        for i, v in enumerate(arr2):
+            
+            while stack2 and arr2[stack2[-1]] < v:
+                current = stack2.pop()
+                res += arr2[current] * (current-stack2[-1]) * (i-current)
+            stack2.append(i)
+            
+        for i, v in enumerate(arr):
+            
+            while stack and arr[stack[-1]] > v:
+                current = stack.pop()
+                res -= arr[current] * (current-stack[-1]) * (i-current)
+            
+            stack.append(i)
+      
+        
+ 
         return res
         
