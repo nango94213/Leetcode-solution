995f740443ab5c6d64eea892b23b67f0b2818bc2
Yinan Yang <845280629abc@gmail.com>
2022-09-06 14:37:24 -0500

    Time: 180 ms (22.27%), Space: 18 MB (51.12%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.py
@@ -0,0 +1,13 @@
+import collections
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        dic = collections.defaultdict(list)
+        
+        for s in strs:
+            dic[tuple(sorted(s))].append(s)
+        
+        return dic.values()
