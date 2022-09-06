50ef26eac4f63b16746e608ea30bd2568d27f513
Yinan Yang <845280629abc@gmail.com>
2022-09-06 17:35:45 -0500

    Time: 256 ms (9.96%), Space: 15.6 MB (71.75%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.py
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -3,27 +3,31 @@ class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         
         indegree = collections.defaultdict(int)
-        outgoing = collections.defaultdict(list)
+        outgoing = collections.defaultdict(set)
         
         for p in prerequisites:
             indegree[p[0]] += 1
-            outgoing[p[1]].append(p[0])
+            outgoing[p[1]].add(p[0])
         
-        stack = []
+        q = collections.deque()
         
-        for i in range(numCourses):
-            if indegree[i] == 0:
-                stack.append(i)
-        
+        for node in range(numCourses):
+            if indegree[node] == 0:
+                q.append(node)
         res = []
         
-        while stack:
-            current = stack.pop()
+        while q:
+            
+            current = q.popleft()
             res.append(current)
+            
             for o in outgoing[current]:
-                    indegree[o] -= 1
-                    
-                    if indegree[o] == 0:
-                        stack.append(o)
+                indegree[o] -= 1
+                
+                if indegree[o] == 0:
+                    q.append(o)
         
         return res if len(res) == numCourses else []
+        
+        
+        
