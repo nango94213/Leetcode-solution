862342b70baff83f79daac4b7d5f9dcabc839d56
Yinan Yang <845280629abc@gmail.com>
2022-09-06 23:20:52 -0500

    Time: 1155 ms (12.07%), Space: 15.1 MB (80.46%) - LeetHub

--- 1730-shortest-path-to-get-food/1730-shortest-path-to-get-food.py
+++ 1730-shortest-path-to-get-food/1730-shortest-path-to-get-food.py
@@ -3,13 +3,12 @@ class Solution:
     def getFood(self, grid: List[List[str]]) -> int:
         
         q = collections.deque()
-        seen = set()
+
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == '*':
                     grid[i][j] = 'X'
                     q.append((i, j))
-                    seen.add((i, j))
                     break
         
         directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
@@ -29,9 +28,10 @@ class Solution:
                 for d in directions:
                     i, j = x + d[0], y + d[1]
                     
-                    if 0 <= i < len(grid) and 0 <= j < len(grid[0]) and grid[i][j] != 'X' and (i, j) not in seen:
+                    if 0 <= i < len(grid) and 0 <= j < len(grid[0]) and grid[i][j] in ('#', 'O'):
                         q.append((i, j))
-                        seen.add((i, j))
+                        if grid[i][j] == 'O':
+                            grid[i][j] = 'X'
             
             step += 1
         
