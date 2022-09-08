0ffd6c847e77404b8d601ad9f4c021db446cdcc8
Yinan Yang <845280629abc@gmail.com>
2022-09-07 23:27:16 -0500

    Time: 124 ms (6.85%), Space: 13.8 MB (80.18%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.py
+++ 12-integer-to-roman/12-integer-to-roman.py
@@ -1,10 +1,14 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        roman={1000: 'M', 900: 'CM', 500: 'D', 400: 'CD', 100: 'C', 90: 'XC', 50: 'L', 40: 'XL', 10: 'X', 9: 'IX', 5: 'V', 4: 'IV', 1: 'I'} 
-        ans=''
-        for k,v in roman.items():
+        roman={1000: 'M', 900: 'CM', 500: 'D', 400: 'CD', 100: 'C', 90: 'XC', 50: 'L', 40: 'XL', 10: 'X', 9: 'IX', 5: 'V', 4: 'IV', 1: 'I'}
+        
+        res = ''
+        
+        for k, v in roman.items():
             if num == 0:
                 break
-            ans += (num//k*v)
-            num%=k
-        return ans
+            
+            res += (num//k*v)
+            num %= k
+        
+        return res
