c8006b7881dfaa96b377d800dd70b5947d5c9f27
Yinan Yang <845280629abc@gmail.com>
2022-09-09 13:57:16 -0500

    Time: 67 ms (9.01%), Space: 14.1 MB (17.28%) - LeetHub

--- 399-evaluate-division/399-evaluate-division.py
+++ 399-evaluate-division/399-evaluate-division.py
@@ -1,23 +1,20 @@
 import collections
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        
         res = []
-        graph = collections.defaultdict(defaultdict)
+        graph = collections.defaultdict(collections.defaultdict)
         
         for e, v in zip(equations, values):
             graph[e[0]][e[1]] = v
             graph[e[1]][e[0]] = 1 / v
-            
+        
         
         def bfs(start, target):
             
             q = collections.deque([(start, 1.0)])
-            
             seen = set([start])
             
             while q:
-                
                 current, value = q.popleft()
                 
                 if current == target:
@@ -25,25 +22,22 @@ class Solution:
                 
                 for neighbor in graph[current]:
                     if neighbor not in seen:
-                        
                         seen.add(neighbor)
-                        q.append((neighbor, value * graph[current][neighbor]))
+                        q.append((neighbor, value*graph[current][neighbor]))
             
-            return  -1.0
+            return -1.0
         
-        for s, t in queries:
-            if s not in graph or t not in graph:
+        
+        for q in queries:
+            if q[0] not in graph or q[1] not in graph:
                 res.append(-1.0)
                 continue
             
-            if s == t:
+            if q[0] == q[1]:
                 res.append(1.0)
                 continue
             
-            res.append(bfs(s, t))
+            res.append(bfs(q[0], q[1]))
         
         return res
-        
-            
-        
         
