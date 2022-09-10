4a519c6d84ad46aa4ee7dfa840bf4ae180ae0223
Yinan Yang <845280629abc@gmail.com>
2022-09-10 18:44:58 -0500

    Time: 2675 ms (5.02%), Space: 33.6 MB (26.68%) - LeetHub

--- 2100-find-good-days-to-rob-the-bank/2100-find-good-days-to-rob-the-bank.py
+++ 2100-find-good-days-to-rob-the-bank/2100-find-good-days-to-rob-the-bank.py
@@ -10,20 +10,22 @@ class Solution:
             
             if security[i] <= security[i-1]:
                 count += 1
-                high.append(count)
             else:
                 count = 0
-                high.append(0)
+                
+                
+            high.append(count)
+            
         
         count = 0
         for i in range(len(security)-2, -1, -1):
             
             if security[i] <= security[i+1]:
                 count += 1
-                low.append(count)
             else:
                 count = 0
-                low.append(0)
+            low.append(count)
+      
             
         low.reverse()
         
