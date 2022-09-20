19d4910db06835de4813a48b1ccd78b8236f1185
Yinan Yang <845280629abc@gmail.com>
2022-09-19 23:47:44 -0500

    Time: 5643 ms (6.27%), Space: 52.9 MB (83.50%) - LeetHub

--- 2055-plates-between-candles/2055-plates-between-candles.py
+++ 2055-plates-between-candles/2055-plates-between-candles.py
@@ -9,8 +9,7 @@ class Solution:
         for q in queries:
             i = bisect.bisect_left(A, q[0])
             j = bisect.bisect_right(A, q[1]) - 1
-            
-            ans = A[j] - A[i] - (j - i) if i < j else 0
-            res.append(ans)
+
+            res.append(A[j] - A[i] - (j - i) if i < j else 0)
         
         return res
