6b86fb2eebffd542ab9d5430233ac61ade43e9df
Yinan Yang <845280629abc@gmail.com>
2022-09-27 10:22:26 -0500

    Time: 341 ms (10.78%), Space: 14 MB (65.14%) - LeetHub

--- 909-snakes-and-ladders/909-snakes-and-ladders.py
+++ 909-snakes-and-ladders/909-snakes-and-ladders.py
@@ -1,31 +1,32 @@
 import collections
 class Solution:
     def snakesAndLadders(self, board: List[List[int]]) -> int:
-        dic = {1:0}
+        seen = set([1])
+        move = 0
         q = collections.deque([1])
         n = len(board)
         while q:
 
-            current = q.popleft()
-            
-            for i in range(current+1, current+7):
+            for _ in range(len(q)):
+                current = q.popleft()
+                if current == n * n:
+                    return move
+                for i in range(current+1, current+7):
                 
-                x = (i-1) // n
-                y = (i-1) % n
+                       x = (i-1) // n
+                       y = (i-1) % n
                 
-               
+                       if 0 <= x < len(board) and 0 <= y < len(board[0]):
    
-                nextN = board[-(x+1)][y if x % 2 == 0 else -(y+1)]
+                                     nextN = board[-(x+1)][y if x % 2 == 0 else -(y+1)]
 
-                if nextN > 0:
-                    i = nextN
-
-                if i == n * n:
-                    return dic[current] + 1
+                                     if nextN > 0:
+                                           i = nextN
                 
-                if i not in dic:
-                    dic[i] = dic[current] + 1
-                    q.append(i)
+                                     if i not in seen:
+                                              seen.add(i)
+                                              q.append(i)
+            move += 1
         
         return -1
             
