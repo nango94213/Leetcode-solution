c7224917e30012302122e72806bc62bdbc15192a
Yinan Yang <845280629abc@gmail.com>
2022-09-30 00:31:14 -0500

    Time: 509 ms (25.31%), Space: 14 MB (54.23%) - LeetHub

--- 723-candy-crush/723-candy-crush.py
+++ 723-candy-crush/723-candy-crush.py
@@ -2,38 +2,35 @@ class Solution:
     def candyCrush(self, board: List[List[int]]) -> List[List[int]]:
         
         m = len(board)
-        n = len(board[0])
-        
-        
-        
+        n = len(board[1])
         
         while True:
-            remove = set()
-            for j in range(n):
-                for i in range(m-2):
-                    if board[i][j] == board[i+1][j] == board[i+2][j] != 0:
-                        remove.update([(i, j), (i+1, j), (i+2, j)])
             
+            remove = set()
             
             for i in range(m):
                 for j in range(n-2):
                     if board[i][j] == board[i][j+1] == board[i][j+2] != 0:
                         remove.update([(i, j), (i, j+1), (i, j+2)])
             
+            for i in range(m-2):
+                for j in range(n):
+                    if board[i][j] == board[i+1][j] == board[i+2][j] != 0:
+                        remove.update([(i, j), (i+1, j), (i+2, j)])
             
-            if not remove:
+            if len(remove) == 0:
                 return board
             
-            
             for j in range(n):
-                falling = 0
+                offset = 0
                 for i in range(m-1, -1, -1):
                     if (i, j) in remove:
-                        falling += 1
+                        offset += 1
                         continue
                     
-                    board[i+falling][j] = board[i][j]
+                    board[i+offset][j] = board[i][j]
                 
-                for i in range(falling):
+                for i in range(offset):
                     board[i][j] = 0
+        
                     
