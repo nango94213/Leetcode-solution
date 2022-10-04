2411aad2b332a403d06c936d04b3d1e4bad2141d
Yinan Yang <845280629abc@gmail.com>
2022-10-04 01:27:00 -0500

    Time: 227 ms (93.75%), Space: 23.4 MB (12.50%) - LeetHub

--- 1444-number-of-ways-of-cutting-a-pizza/1444-number-of-ways-of-cutting-a-pizza.py
+++ 1444-number-of-ways-of-cutting-a-pizza/1444-number-of-ways-of-cutting-a-pizza.py
@@ -1,39 +1,48 @@
-from functools import lru_cache
+MOD = int(1e9+7)
+
 class Solution:
-    def ways(self, pizza: List[str], K: int) -> int:
-        MOD = 10 ** 9 + 7
-        n, m = len(pizza), len(pizza[0])
-        num_apples = [[0] * (m + 1) for _ in range(n + 1)]
-        for i in range(n - 1, -1, -1):
-            for j in range(m - 1, -1, -1):
-                num_apples[i][j] = (j + 1 < m and num_apples[i][j + 1]) + \
-                                   (i + 1 < n and num_apples[i + 1][j]) - \
-                                   (i + 1 < n and j + 1 < m and num_apples[i + 1][j + 1]) + \
-                                   int(pizza[i][j] == 'A')
-                
-        def is_valid(x0, y0, x1=n-1, y1=m-1):
-            '''inclusive'''
-            if x0 > x1 or y0 > y1: return False
-            num = num_apples[x0][y0] - \
-                  num_apples[x1 + 1][y0] - \
-                  num_apples[x0][y1 + 1] + \
-                  num_apples[x1 + 1][y1 + 1]
-            return num > 0
-        
+    def ways(self, pizza: List[str], k: int) -> int:
+        m, n = len(pizza), len(pizza[0])
         @cache
-        def num_ways(x, y, k):
-            if not is_valid(x, y): return 0
-            if k == 1: return 1
-            res = 0
-            # cut horizontally
-            for j in range(y, m - 1):
-                if is_valid(x, y, n - 1, j):
-                    res = (res + num_ways(x, j + 1, k - 1)) % MOD
-            # cut vertically                    
-            for i in range(x, n - 1):
-                if is_valid(x, y, i, m - 1):
-                    res = (res + num_ways(i + 1, y, k - 1)) % MOD
-            return res
-        
-        return num_ways(0, 0, K)
-        
+        def has_apple(row, col, rowflag) -> bool:
+            """Return if a row or col has any apple.
+            
+            If rowflag is True, we search the row [(row, col), (row, col+1), ..., (row, n-1)]
+            If rowflag is False, we search the col [(row, col), (row+1, col), ..., (m-1, col)]
+            """
+            if row >= m or col >= n:
+                return False
+            elif pizza[row][col] == 'A':
+                return True
+            elif rowflag:
+                return has_apple(row, col+1, rowflag)
+            else:
+                return has_apple(row+1, col, rowflag)
+                
+        @cache
+        def count(top: int, left: int, k: int, cuttable: bool, direc: int) -> int:
+            """Return num of ways of cutting.
+            
+            For the submatrix with (top, left) and (m-1, n-1) as top-left and bottom-right points,
+            k = remaining pieces. When k = 1 means we can decide if it's a valid cutting.
+            cuttable = if we can take a cut now. If we cut, the current submatrix is left
+                       for further cutting.
+            direc =
+                0 -- arbitrary, immediately after a cut
+                1 -- we want to cut horizontally
+                2 -- we want to cut vertically
+            """
+            if top == m or left == n:
+                return 0
+            elif k == 1:
+                return 1 if any(has_apple(row, left, True) for row in range(top, m)) else 0
+            else:
+                # Try cutting if cuttable at this point
+                total = count(top, left, k-1, False, 0) if cuttable else 0
+                # Try next row or col without cutting
+                if not direc or direc == 1:
+                    total += count(top+1, left, k, cuttable or has_apple(top, left, True), 1)
+                if not direc or direc == 2:
+                    total += count(top, left+1, k, cuttable or has_apple(top, left, False), 2)
+                return total % MOD
+        return count(0, 0, k, False, 0)
