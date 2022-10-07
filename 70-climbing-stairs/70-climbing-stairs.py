f357486dcdd2ffca31d305cd3a031ab5079e1c61
Yinan Yang <845280629abc@gmail.com>
2022-10-07 02:24:36 -0500

    Time: 58 ms (29.19%), Space: 13.8 MB (56.93%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -9,7 +9,7 @@ class Solution:
         if n == 1:
             return 1
         
-        for i in range(2, n+1):
+        for _ in range(n-1):
             current = pre_two + pre_one
             
             pre_one, pre_two = current, pre_one
