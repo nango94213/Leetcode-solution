edffd0609c505810cadc69de4495694776c267b2
Yinan Yang <845280629abc@gmail.com>
2022-10-08 22:20:43 -0500

    Time: 54 ms (63.35%), Space: 13.9 MB (79.80%) - LeetHub

--- 314-binary-tree-vertical-order-traversal/314-binary-tree-vertical-order-traversal.py
+++ 314-binary-tree-vertical-order-traversal/314-binary-tree-vertical-order-traversal.py
@@ -4,30 +4,30 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
 import collections
-
 class Solution:
     def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        
         if not root:
             return []
         
-        columnData = collections.defaultdict(list)
-        
         q = collections.deque([(root, 0)])
+        
+        dic = collections.defaultdict(list)
         
         while q:
             
-            node, column = q.popleft()
-            
-            columnData[column].append(node.val)
-            
-            if node.left:
-                q.append((node.left, column-1))
-            
-            if node.right:
-                q.append((node.right, column+1))
+            for _ in range(len(q)):
+                
+                current, x = q.popleft()
+                
+                dic[x].append(current.val)
+                
+                if current.left:
+                    q.append((current.left, x-1))
+                
+                if current.right:
+                    q.append((current.right, x+1))
         
-        return [columnData[x] for x in sorted(columnData.keys())]
+        return [dic[key] for key in sorted(dic.keys())]
+        
         
