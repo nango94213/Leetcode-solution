2081507057b1d3c23d365a0734f33a3800dc1a52
Yinan Yang <845280629abc@gmail.com>
2022-10-22 23:09:20 -0500

    Time: 1745 ms (22.91%), Space: 22 MB (26.25%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.py
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.py
@@ -3,41 +3,35 @@ class Solution:
     def shortestPath(self, grid: List[List[int]], k: int) -> int:
         
         directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-        
+        steps = 0
+        target = (len(grid)-1, len(grid[0])-1)
         q = collections.deque([(0, 0, k)])
         seen = set([(0, 0, k)])
         
-        rows = len(grid)
-        cols = len(grid[0])
-        
-        target = (rows-1, cols-1)
-        
-        steps = 0
+        m = len(grid)
+        n = len(grid[0])
         
         while q:
-            
             for _ in range(len(q)):
-                
                 x, y, live = q.popleft()
                 
                 if (x, y) == target:
-                    
                     return steps
                 
                 for d in directions:
+                    newX = x + d[0]
+                    newY = y + d[1]
                     
-                    newx, newy = x + d[0], y + d[1]
-                    
-                    if 0 <= newx < rows and 0 <= newy < cols:
+                    if 0 <= newX < m and 0 <= newY < n:
                         
-                        if grid[newx][newy] != 1 and (newx, newy, live) not in seen:
-                            q.append((newx, newy, live))
-                            seen.add((newx, newy, live))
+                        if grid[newX][newY] == 0 and (newX, newY, live) not in seen:
+                            seen.add((newX, newY, live))
+                            q.append((newX, newY, live))
                         
-                        if grid[newx][newy] == 1 and (live-1) >=0 and (newx, newy, live-1) not in seen:
-                            q.append((newx, newy, live-1))
-                            seen.add((newx, newy, live-1))
-            
+                        if grid[newX][newY] == 1 and live > 0 and (newX, newY, live-1) not in seen:
+                            seen.add((newX, newY, live-1))
+                            q.append((newX, newY, live-1))
+                
             steps += 1
         
         return -1
