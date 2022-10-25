312bfb58409940bde3bc84a61b82714ec41a103d
Yinan Yang <845280629abc@gmail.com>
2022-10-24 23:40:18 -0500

    Time: 855 ms (51.03%), Space: 14.6 MB (86.53%) - LeetHub

--- 0159-longest-substring-with-at-most-two-distinct-characters/0159-longest-substring-with-at-most-two-distinct-characters.py
+++ 0159-longest-substring-with-at-most-two-distinct-characters/0159-longest-substring-with-at-most-two-distinct-characters.py
@@ -10,12 +10,12 @@ class Solution:
             
             seen[s[right]] += 1
             
-            while len(seen) > 2:
+            if len(seen) > 2:
                 seen[s[left]] -= 1
                 if seen[s[left]] == 0:
                     del seen[s[left]]
                 left += 1
-            res = max(res, right - left + 1)
+            
    
-        return res
+        return right - left + 1
         
