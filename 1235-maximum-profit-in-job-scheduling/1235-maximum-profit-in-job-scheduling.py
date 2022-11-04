6b2d44f54a326bbb8ec745c9aa89387d7ae78c74
Yinan Yang <845280629abc@gmail.com>
2022-11-03 23:38:48 -0500

    Time: 1363 ms (52.70%), Space: 27.4 MB (68.57%) - LeetHub

---
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
@@ -0,0 +1,15 @@
+import bisect
+class Solution:
+    def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
+        
+        dp = [[0, 0]]
+        
+        info = sorted(zip(startTime, endTime, profit), key=lambda x: x[1])
+        
+        for s, e, p in info:
+            index = bisect.bisect_right(dp, [s+1]) - 1
+    
+            if dp[index][1] + p > dp[-1][-1]:
+                dp.append([e, dp[index][1] + p])
+        
+        return dp[-1][-1]
