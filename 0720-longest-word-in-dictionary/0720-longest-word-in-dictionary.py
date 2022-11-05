45309e170ee69cbe94796732966850d8972c58ff
Yinan Yang <845280629abc@gmail.com>
2022-11-05 14:30:27 -0500

    Time: 322 ms (35.98%), Space: 15.1 MB (43.20%) - LeetHub

--- 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.py
+++ 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.py
@@ -10,13 +10,15 @@ class Solution:
             reduce(dict.__getitem__, word, trie)[END] = i
         
         stack = list(trie.values())
-        
+
         ans = ""
         
         while stack:
             cur = stack.pop()
             if END in cur:
+    
                 word = words[cur[END]]
+              
                 if len(word) > len(ans) or len(word) == len(ans) and word < ans:
                     ans = word
                 stack.extend([cur[letter] for letter in cur if letter != END])
