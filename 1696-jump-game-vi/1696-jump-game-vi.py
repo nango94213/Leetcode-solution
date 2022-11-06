fb35e12ce894de726c06e72a581b5fa5627c8012
Yinan Yang <845280629abc@gmail.com>
2022-11-05 19:35:11 -0500

    Time: 2334 ms (58.07%), Space: 27.6 MB (71.21%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.py
+++ 1696-jump-game-vi/1696-jump-game-vi.py
@@ -2,23 +2,19 @@ import collections
 class Solution:
     def maxResult(self, nums: List[int], k: int) -> int:
         
-        dp = [0] * len(nums)
+        dp = [-float('inf')] * len(nums)
         dp[0] = nums[0]
         
         q = collections.deque([0])
         
         for i in range(1, len(nums)):
-            
             if q and q[0] < i - k:
                 q.popleft()
-            
             dp[i] = dp[q[0]] + nums[i]
             
             while q and dp[q[-1]] < dp[i]:
                 q.pop()
-            
             q.append(i)
         
         return dp[-1]
-        
         
