d212a6a059412f08ca6332741fe80573c01b6045
Yinan Yang <845280629abc@gmail.com>
2022-11-06 01:17:33 -0600

    Time: 74 ms (42.47%), Space: 13.9 MB (81.23%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
@@ -5,17 +5,17 @@ class Solution:
         dic = {}
         for i, v in enumerate(s):
             dic[v] = i
-            
+        
         seen = set()
         
         for i, v in enumerate(s):
             if v not in seen:
+                while res and res[-1] > v and dic[res[-1]] > i:
+                    tmp = res.pop()
+                    seen.remove(tmp)
                 
-                while res and res[-1] > v and i < dic[res[-1]]:
-                    letter = res.pop()
-                    seen.remove(letter)
-                    
-                res.append(v)
                 seen.add(v)
+                res.append(v)
         
         return ''.join(res)
+                    
