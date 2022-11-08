67392b7038a2fccf2c3b46d6027d9460de143745
Yinan Yang <845280629abc@gmail.com>
2022-11-08 15:42:42 -0600

    Time: 5051 ms (43.24%), Space: 60.3 MB (28.23%) - LeetHub

--- 2158-amount-of-new-area-painted-each-day/2158-amount-of-new-area-painted-each-day.py
+++ 2158-amount-of-new-area-painted-each-day/2158-amount-of-new-area-painted-each-day.py
@@ -1,23 +1,19 @@
 class Solution:
     def amountPainted(self, paint: List[List[int]]) -> List[int]:
-        
+        p = [0] * 500000
         res = []
-        done = [0]  * 500000
-        
         for s, e in paint:
-            
             count = 0
             
             while s < e:
-                if done[s] == 0:
+                if p[s] == 0:
                     count += 1
-                    done[s] = e
+                    p[s] = e
                     s += 1
                 else:
-                    tmp = s
-                    s = done[s]
-                    done[tmp] = max(done[tmp], e)
-            
+                    t = s
+                    s = p[s]
+                    p[t] = max(p[t], e)
             res.append(count)
         
         return res
