57a4f1e77412c7f5c1f8146cd261755b82d3b46b
Yinan Yang <845280629abc@gmail.com>
2022-11-18 02:15:30 -0600

    Time: 3841 ms (74.61%), Space: 121.5 MB (26.17%) - LeetHub

--- 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.py
+++ 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.py
@@ -1,40 +1,34 @@
 import collections
 class Solution:
     def largestPathValue(self, colors: str, edges: List[List[int]]) -> int:
-        
-        indegree = collections.defaultdict(int)
         outgoing = collections.defaultdict(set)
-        
-        for e in edges:
-            indegree[e[1]] += 1
-            outgoing[e[0]].add(e[1])
+        indegree = collections.defaultdict(int)
         
         dp = [[0]*26 for _ in range(len(colors))]
         
-        processed = 0
+        for e in edges:
+            outgoing[e[0]].add(e[1])
+            indegree[e[1]] += 1
         
         q = collections.deque()
         
         for i in range(len(colors)):
             if indegree[i] == 0:
                 q.append(i)
+        
         res = 0
+        count = 0
         while q:
             current = q.popleft()
-            processed += 1
+            count += 1
             index = ord(colors[current]) - ord('a')
-            
             dp[current][index] += 1
-            
             res = max(res, dp[current][index])
             
             for o in outgoing[current]:
+                dp[o] = [max(i, j) for i, j in zip(dp[current], dp[o])]
                 indegree[o] -= 1
-                
-                dp[o] = [max(a, b) for a, b in zip(dp[current], dp[o])]
-                
                 if indegree[o] == 0:
                     q.append(o)
         
-        return res if processed == len(colors) else -1
-        
+        return res if count == len(colors) else -1
