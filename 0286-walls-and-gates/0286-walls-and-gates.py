80caa86b1595ecef14404262041a3959f7f239a7
Yinan Yang <845280629abc@gmail.com>
2022-11-19 17:44:18 -0600

    Time: 758 ms (37.96%), Space: 17.1 MB (68.80%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.py
+++ 0286-walls-and-gates/0286-walls-and-gates.py
@@ -1,39 +1,36 @@
-import collections
 class Solution:
     def wallsAndGates(self, rooms: List[List[int]]) -> None:
         """
         Do not return anything, modify rooms in-place instead.
         """
-        if not rooms:
-            return 
         directions = [(0, -1), (-1, 0), (0, 1), (1, 0)]
+        if not rooms:
+            return []
+        
+        q = collections.deque()
         
         m = len(rooms)
         n = len(rooms[0])
-        
-        q = collections.deque()
         
         for i in range(m):
             for j in range(n):
                 if rooms[i][j] == 0:
                     q.append((i, j))
         
-        jump = 0
+        jump = 1
         
         while q:
-            
             for _ in range(len(q)):
-                
-                current = q.popleft()
+                x, y = q.popleft()
                 
                 for d in directions:
-                    i = current[0] + d[0]
-                    j = current[1] + d[1]
+                    i = x + d[0]
+                    j = y + d[1]
                     
                     if 0 <= i < m and 0 <= j < n and rooms[i][j] == 2147483647:
-                        rooms[i][j] = jump + 1
+                        rooms[i][j] = jump
                         q.append((i, j))
+            
             jump += 1
         
-                
-        
+        return rooms
