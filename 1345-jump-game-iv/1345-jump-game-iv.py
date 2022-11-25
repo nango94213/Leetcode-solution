095bf0206823f8fc9b60a4eb5fd0d683df55c20b
Yinan Yang <845280629abc@gmail.com>
2022-11-24 21:56:28 -0600

    Time: 2488 ms (27.32%), Space: 35.9 MB (6.78%) - LeetHub

--- 1345-jump-game-iv/1345-jump-game-iv.py
+++ 1345-jump-game-iv/1345-jump-game-iv.py
@@ -1,46 +1,41 @@
 import collections
 class Solution:
     def minJumps(self, arr: List[int]) -> int:
-        
-        graph = collections.defaultdict(set)
+        group = collections.defaultdict(set)
         
         for i, v in enumerate(arr):
-            graph[v].add(i)
-            
-        target = len(arr) - 1
+            group[v].add(i)
         
         q = collections.deque([0])
+        
         seen = set([0])
         
         jump = 0
         
         while q:
             for _ in range(len(q)):
-                
                 current = q.popleft()
                 
-                if current == target:
+                if current == len(arr) - 1:
                     return jump
                 
-                left = current - 1
-                right = current + 1
-                same = graph[arr[current]]
+                if current + 1 < len(arr) and current + 1 not in seen:
+                    q.append(current+1)
+                    seen.add(current+1)
                 
-                if left >= 0 and left not in seen:
-                    seen.add(left)
-                    q.append(left)
+                if current - 1 >= 0 and current - 1 not in seen:
+                    q.append(current-1)
+                    seen.add(current-1)
+                    
+                for o in group[arr[current]]:
+                    if o not in seen:
+                        q.append(o)
+                        seen.add(o)
                 
-                if right <= target and right not in seen:
-                    seen.add(right)
-                    q.append(right)
-                
-                for s in same:
-                    if s not in seen:
-                        seen.add(s)
-                        q.append(s)
-                
-                del graph[arr[current]]
+                del group[arr[current]]
             
             jump += 1
         
-         
+        return jump
+        
+        
