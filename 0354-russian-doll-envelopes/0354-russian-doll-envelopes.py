f3dc25c59d424091b02d5d1d107d46936aba4009
Yinan Yang <845280629abc@gmail.com>
2022-11-27 13:53:23 -0600

    Time: 3080 ms (32.22%), Space: 61.9 MB (26.82%) - LeetHub

--- 0354-russian-doll-envelopes/0354-russian-doll-envelopes.py
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.py
@@ -1,23 +1,17 @@
 import bisect
 class Solution:
     def maxEnvelopes(self, envelopes: List[List[int]]) -> int:
-        
         envelopes.sort(key=lambda x: (x[0], -x[1]))
         
-        check = [i[1] for i in envelopes]
-        
+      
         res = []
         
-        for number in check:
-            index = bisect.bisect_left(res, number)
+        for e in [i[1] for i in envelopes]:
+            index = bisect.bisect_left(res, e)
             
-            if not res:
-                res.append(number)
-                continue
-            if number > res[-1]:
-                res.append(number)
+            if index == len(res):
+                res.append(e)
             else:
-                res[index] = number
+                res[index] = e
         
         return len(res)
-        
