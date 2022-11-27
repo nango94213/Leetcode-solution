5be57f7a6414fc697c9c9cb0ad8620d3a145a657
Yinan Yang <845280629abc@gmail.com>
2022-11-26 21:48:52 -0600

    Time: 87 ms (5.05%), Space: 13.9 MB (80.85%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -2,20 +2,17 @@ class Solution:
     def numDecodings(self, s: str) -> int:
         if s[0] == '0':
             return 0
-        
-        prev1 = 1
+        if len(s) == 1:
+            return 1
         prev2 = 1
+        prev1 = 1
         
         for i in range(1, len(s)):
             current = 0
-            
             if s[i] != '0':
                 current += prev1
-            
-            if int(s[i-1:i+1]) >= 10 and int(s[i-1:i+1]) <= 26:
+            if 10 <= int(s[i-1:i+1]) <= 26:
                 current += prev2
-            
             prev1, prev2 = current, prev1
         
-        return prev1
-        
+        return current
