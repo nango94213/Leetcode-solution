f5982fb827129c8dc44421049cbd3aee51482ffc
Yinan Yang <845280629abc@gmail.com>
2022-11-27 15:19:48 -0600

    Time: 110 ms (75.07%), Space: 14.2 MB (61.30%) - LeetHub

--- 1366-rank-teams-by-votes/1366-rank-teams-by-votes.py
+++ 1366-rank-teams-by-votes/1366-rank-teams-by-votes.py
@@ -1,11 +1,12 @@
 import collections
 class Solution:
     def rankTeams(self, votes: List[str]) -> str:
-        dic = collections.defaultdict(lambda: [0 for i in range(len(votes[0]))])
+        info = collections.defaultdict(lambda :[0 for _ in range(len(votes[0]))])
         
-        for v in votes:
-            for i, c in enumerate(v):
-                dic[c][i] += 1
-     
-        return ''.join(sorted(dic.keys(), key=lambda x: (dic[x], - ord(x)), reverse = True))
-                
+        for i in range(len(votes)):
+            for j, v in enumerate(votes[i]):
+                info[v][j] += 1
+        
+
+        return ''.join(sorted(info.keys(), key=lambda x: (info[x], -ord(x)), reverse=True))
+        
