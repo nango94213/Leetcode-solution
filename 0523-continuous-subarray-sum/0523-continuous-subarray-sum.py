7519d4e9bef2654b07de5ec1cce418ece1cf3cbc
Yinan Yang <845280629abc@gmail.com>
2022-11-27 17:51:02 -0600

    Time: 2516 ms (38.19%), Space: 33.2 MB (35.20%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -1,19 +1,14 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
-        
+        dic = {0: -1}
         current = 0
         
-        dic = {0: -1}
-        
-        for i in range(len(nums)):
-            current += nums[i]
+        for i, v in enumerate(nums):
+            current += v
+            if current%k in dic and i - dic[current%k] >= 2:
+                return True
             
-            if current % k in dic:
-                if i - dic[current%k] + 1 > 2:
-                    return True
-            else:
+            if current%k not in dic:
                 dic[current%k] = i
-                
         
         return False
-        
