fcba1a2b63fba7985d24c54631248cd948e83a0e
Yinan Yang <845280629abc@gmail.com>
2022-11-27 12:48:12 -0600

    Time: 63 ms (40.25%), Space: 14 MB (5.88%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,25 +1,17 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         
+        def house(num):
+   
+            prev2 = 0
+            prev1 = num[0]
+            
+            for i in range(1, len(num)):
+                current = max(prev2+num[i], prev1)
+                prev1, prev2 = current, prev1
+            
+            return prev1
+        
         if len(nums) == 1:
             return nums[0]
-        
-        if len(nums) == 2:
-            return max(nums[0], nums[1])
-        
-        two = nums[0]
-        one = max(nums[0], nums[1])
-        
-        for i in range(2, len(nums)-1):
-            two, one = one, max(two+nums[i], one)
-        
-        
-       
-        
-        two1 = nums[1]
-        one1 = max(nums[1], nums[2])
-        
-        for i in range(3, len(nums)):
-            two1, one1 = one1, max(two1+nums[i], one1)
-            
-        return max(one, one1)
+        return max(house(nums[:-1]), house(nums[1:]))
