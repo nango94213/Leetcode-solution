a1e95843bb28aa210dfa7c80120a362891cc7b94
Yinan Yang <845280629abc@gmail.com>
2022-11-27 14:52:15 -0600

    Time: 3252 ms (12.61%), Space: 27.7 MB (57.66%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.py
+++ 1696-jump-game-vi/1696-jump-game-vi.py
@@ -1,15 +1,15 @@
+
 import collections
 class Solution:
     def maxResult(self, nums: List[int], k: int) -> int:
         
         dp = [-float('inf')] * len(nums)
         dp[0] = nums[0]
-        
         q = collections.deque([0])
-        
         for i in range(1, len(nums)):
             if q and q[0] < i - k:
                 q.popleft()
+          
             dp[i] = dp[q[0]] + nums[i]
             
             while q and dp[q[-1]] < dp[i]:
@@ -17,4 +17,3 @@ class Solution:
             q.append(i)
         
         return dp[-1]
-        
