ee42f83cdf16700eb4c6269933cb80e8a1a23a34
Yinan Yang <845280629abc@gmail.com>
2022-11-27 02:06:15 -0600

    Time: 6626 ms (5.11%), Space: 13.9 MB (76.07%) - LeetHub

--- 1774-closest-dessert-cost/1774-closest-dessert-cost.py
+++ 1774-closest-dessert-cost/1774-closest-dessert-cost.py
@@ -1,16 +1,15 @@
 class Solution:
     def closestCost(self, baseCosts: List[int], toppingCosts: List[int], target: int) -> int:
-        
         res = float('inf')
         
         def dfs(pool, path):
             nonlocal res
-            if abs(path - target) < abs(res - target):
+         
+            if abs(path-target) < abs(res-target):
                 res = path
-            
-            if abs(path - target) == abs(res - target):
+                print(path)
+            if abs(path-target) == abs(res-target):
                 res = min(res, path)
-            
             if path > target:
                 return
             
@@ -19,8 +18,8 @@ class Solution:
                 dfs(pool[i+1:], path+pool[i])
                 dfs(pool[i+1:], path+2*pool[i])
         
-        
-        for base in baseCosts:
-            dfs(toppingCosts, base)
+        for b in baseCosts:
+            dfs(toppingCosts, b)
         
         return res
+        
