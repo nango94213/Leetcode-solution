aba8fcd4656a8ab6126e749e55f6ba621c763c33
Yinan Yang <845280629abc@gmail.com>
2022-11-27 03:20:15 -0600

    Time: 36 ms (89.02%), Space: 14 MB (42.99%) - LeetHub

--- 0722-remove-comments/0722-remove-comments.py
+++ 0722-remove-comments/0722-remove-comments.py
@@ -1,30 +1,24 @@
 class Solution:
     def removeComments(self, source: List[str]) -> List[str]:
         res = []
-        buffer = ''
-        inblock = False
-        
+        current = ''
+        block = False
         for s in source:
             i = 0
             while i < len(s):
-                if s[i] == '/' and (i+1) < len(s) and s[i+1] == '/' and not inblock:
+                if s[i] == '/' and i+1 < len(s) and s[i+1] == '/' and not block:
                     i = len(s)
-                elif s[i] == '/' and (i+1) < len(s) and s[i+1] == '*' and not inblock:
-                    inblock = True
+                elif s[i] == '/' and i+1 < len(s) and s[i+1] == '*' and not block:
+                    block = True
                     i += 1
-                elif s[i] == '*' and (i+1) < len(s) and s[i+1] == '/' and inblock:
-                    inblock = False
+                elif s[i] == '*' and i+1 < len(s) and s[i+1] == '/' and block:
+                    block = False
                     i += 1
-                elif not inblock:
-                    buffer += s[i]
-                
+                elif not block:
+                    current += s[i]
                 i += 1
             
-            if buffer and not inblock:
-                res.append(buffer)
-                buffer = ''
-        
+            if current and not block:
+                res.append(current)
+                current = ''
         return res
-                    
-                    
-        
