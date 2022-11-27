0685795b73d5f2e7d8b9dd9432250fd04852b236
Yinan Yang <845280629abc@gmail.com>
2022-11-26 18:05:07 -0600

    Time: 2138 ms (5.74%), Space: 31 MB (15.18%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -0,0 +1,15 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        res = []
+        
+        def dfs(pool, path):
+            
+            if not pool:
+                res.append(path)
+            
+            for i in range(len(pool)):
+                if pool[:i+1] == pool[:i+1][::-1]:
+                    dfs(pool[i+1:], path+[pool[:i+1]])
+            
+        dfs(s, [])
+        return res
