9b9a73b99f3e3ffe0d03d0d504d4bf5c18d8d139
Yinan Yang <845280629abc@gmail.com>
2022-11-27 12:16:36 -0600

    Time: 1120 ms (41.51%), Space: 61.7 MB (14.91%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
@@ -2,31 +2,31 @@ import random
 class RandomizedSet:
 
     def __init__(self):
-        self.dic = {}
         self.stack = []
+        self.dic = {}
+        
 
     def insert(self, val: int) -> bool:
         if val in self.dic:
             return False
-        self.dic[val] = len(self.stack)
         self.stack.append(val)
+        self.dic[val] = len(self.stack) - 1
         return True
 
     def remove(self, val: int) -> bool:
         if val not in self.dic:
             return False
-        
         index = self.dic[val]
-        self.stack[index] = self.stack[-1]
-        self.dic[self.stack[-1]] = index
-        
+        last = self.stack[-1]
+        self.stack[index] = last
         self.stack.pop()
+        self.dic[last] = index
         del self.dic[val]
+        
         return True
 
     def getRandom(self) -> int:
         return random.choice(self.stack)
-        
 
 
 # Your RandomizedSet object will be instantiated and called as such:
