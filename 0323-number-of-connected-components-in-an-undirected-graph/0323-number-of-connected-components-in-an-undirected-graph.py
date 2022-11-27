f908ba7f5bfa5a9e9a14237ed6a0870f92c2e333
Yinan Yang <845280629abc@gmail.com>
2022-11-27 14:16:16 -0600

    Time: 130 ms (79.47%), Space: 15.7 MB (50.81%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
@@ -1,29 +1,27 @@
 import collections
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        
-        dic = collections.defaultdict(set)
+        graph = collections.defaultdict(set)
         
         for e in edges:
-            dic[e[0]].add(e[1])
-            dic[e[1]].add(e[0])
+            graph[e[0]].add(e[1])
+            graph[e[1]].add(e[0])
         
         seen = set()
         
-        res = 0
+        count = 0
         
         for i in range(n):
             if i not in seen:
-                res += 1
+                count += 1
                 seen.add(i)
                 q = collections.deque([i])
                 
                 while q:
                     current = q.popleft()
                     
-                    for o in dic[current]:
+                    for o in graph[current]:
                         if o not in seen:
                             seen.add(o)
                             q.append(o)
-        
-        return res
+        return count
