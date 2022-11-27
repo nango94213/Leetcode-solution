2fd754b8a72a9ebcb77a12812693a32e97bab04f
Yinan Yang <845280629abc@gmail.com>
2022-11-27 03:28:56 -0600

    Time: 8739 ms (5.22%), Space: 17.1 MB (90.23%) - LeetHub

--- 0855-exam-room/0855-exam-room.py
+++ 0855-exam-room/0855-exam-room.py
@@ -1,30 +1,28 @@
+import bisect
 class ExamRoom:
 
     def __init__(self, n: int):
+        self.seats = []
         self.n = n
-        self.student = []
-        
 
     def seat(self) -> int:
-        if not self.student:
-            self.student.append(0)
+        if not self.seats:
+            self.seats.append(0)
             return 0
+        s = 0
+        d = self.seats[0]
         
-        d, res = self.student[0], 0
-        
-        for a, b in zip(self.student, self.student[1:]):
+        for a, b in zip(self.seats, self.seats[1:]):
             if (b-a) // 2 > d:
-                d, res = (b-a) // 2, (a+b) // 2
-
-        if (self.n-1) - self.student[-1] > d:
-            res = self.n - 1
-        bisect.insort(self.student, res)
-        return res
-        
+                s = (a+b) // 2
+                d = (b-a) // 2
+        if self.n - 1 - self.seats[-1] > d:
+            s = self.n - 1
+        bisect.insort(self.seats, s)
+        return s
 
     def leave(self, p: int) -> None:
-        self.student.remove(p)
-        
+        self.seats.remove(p)
 
 
 # Your ExamRoom object will be instantiated and called as such:
