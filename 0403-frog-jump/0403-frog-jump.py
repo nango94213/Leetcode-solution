8d05a17ef5f8b5caeaa9e8a318c1304b2da48552
Yinan Yang <845280629abc@gmail.com>
2022-11-27 02:36:20 -0600

    Time: 1127 ms (25.76%), Space: 22.6 MB (44.28%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.py
+++ 0403-frog-jump/0403-frog-jump.py
@@ -1,32 +1,26 @@
 import collections
 class Solution:
     def canCross(self, stones: List[int]) -> bool:
-        if stones[1]!=1:
+        if stones[1] != 1:
             return False
-            
-        
-        stack = collections.deque([(1,1)])
-        
+        q = collections.deque([(1, 1)])
         seen = set([(1, 1)])
-        target = stones[-1]
-        stones_set = set(stones)
-        while stack:
-           
-                k, p = stack.popleft()
-                if p == target:
-                    return True
+        s = set(stones)
+        while q:
+        
+            current, k = q.popleft()
+            if current == stones[-1]:
+                return True
             
-                if (p+k) in stones_set and (k, p+k) not in seen:
-                    seen.add((k, p+k))
-                    stack.append((k, p+k))
-                
-                if (p+k+1) in stones_set and (k+1, p+k+1) not in seen:
-                    seen.add((k+1, p+k+1))
-                    stack.append((k+1, p+k+1))
-                
-                if (p+k-1) in stones_set and (k-1, p+k-1) not in seen:
-                    seen.add((k-1, p+k-1))
-                    stack.append((k-1, p+k-1))
+            if current+k in s and (current+k, k) not in seen:
+                seen.add((current+k, k))
+                q.append((current+k, k))
+            if current+k+1 in s and (current+k+1, k+1) not in seen:
+                seen.add((current+k+1, k+1))
+                q.append((current+k+1, k+1))            
+            if current+k-1 in s and (current+k-1, k-1) not in seen:
+                seen.add((current+k-1, k-1))
+                q.append((current+k-1, k-1))
         
         return False
-        
+            
