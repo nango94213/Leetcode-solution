60e1d3be8b4d59b7ad759f259b619d7823f7431b
Yinan Yang <845280629abc@gmail.com>
2022-11-26 22:23:06 -0600

    Time: 1088 ms (72.83%), Space: 16.9 MB (68.61%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.py
+++ 0221-maximal-square/0221-maximal-square.py
@@ -13,8 +13,8 @@ class Solution:
                         dp[i][j] = 1
                     else:
                         dp[i][j] = min(dp[i-1][j-1], dp[i-1][j], dp[i][j-1]) + 1
-                    res = max(res, dp[i][j] * dp[i][j])
+                    res = max(res, dp[i][j])
         
-        return res
+        return res**2
                     
         
