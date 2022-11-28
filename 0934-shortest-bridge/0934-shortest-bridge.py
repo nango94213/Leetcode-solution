e64b1077c862836d14595c9f4b18f1f74d11369e
Yinan Yang <845280629abc@gmail.com>
2022-11-28 01:05:18 -0600

    Time: 1062 ms (48.62%), Space: 16.7 MB (69.22%) - LeetHub

--- 0934-shortest-bridge/0934-shortest-bridge.py
+++ 0934-shortest-bridge/0934-shortest-bridge.py
@@ -1,3 +1,4 @@
+import collections
 class Solution:
     def shortestBridge(self, grid: List[List[int]]) -> int:
         
@@ -5,15 +6,7 @@ class Solution:
         n = len(grid[0])
         directions = [(1,0),(-1,0),(0,1),(0,-1)]
         
-
-        
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == 1:
-                    x, y = i, j
-                    break
-        
-        bound = collections.deque()
+        q = collections.deque()
         
         def dfs(x, y):
             grid[x][y] = -1
@@ -23,30 +16,40 @@ class Solution:
                 j = y + d[1]
                 
                 if 0 <= i < m and 0 <= j < n:
-                    if grid[i][j] == 0:
-                        grid[i][j] = -1
-                        bound.append((i, j))
-                    elif grid[i][j] == 1:
+                    if grid[i][j] == 1:
                         dfs(i, j)
-        dfs(x, y)
-        jump = 0
-     
-        while bound:
-            for _ in range(len(bound)):
+                    elif grid[i][j] == 0:
+                        grid[i][j] = -1
+                        q.append((i, j))
+        stop = False
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    dfs(i, j)
+                    stop = True
+                    break
+            if stop:
+                break
+        
+        step = 0
+        while q:
+            for _ in range(len(q)):
+                x, y = q.popleft()
                 
-                current = bound.popleft()
-                
-                if grid[current[0]][current[1]] == 1:
-                    return jump
+                if grid[x][y] == 1:
+                    return step
                 
                 for d in directions:
-                    i = current[0] + d[0]
-                    j = current[1] + d[1]
-                    
-                    if 0 <= i < m and 0 <= j < n and grid[i][j] != -1:
+                    i = x + d[0]
+                    j = y + d[1]
+                    if 0 <= i < m and 0 <= j < n and grid[i][j] in (0, 1):
                         if grid[i][j] == 0:
                             grid[i][j] = -1
-                        bound.append((i, j))
-            jump += 1
-        
+                        q.append((i, j))
+            step += 1
         return -1
+                        
+                    
+        
+        
+        
