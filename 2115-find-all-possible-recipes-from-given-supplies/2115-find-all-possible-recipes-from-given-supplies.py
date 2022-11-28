bbf4ff4a45d0fe4082144d516b51044b44f3ff6a
Yinan Yang <845280629abc@gmail.com>
2022-11-28 09:50:30 -0600

    Time: 2403 ms (41.94%), Space: 17 MB (68.50%) - LeetHub

--- 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
@@ -2,23 +2,22 @@ import collections
 class Solution:
     def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
         
-        rset = set(recipes)
-        sset = set(supplies)
-        
         indegree = collections.defaultdict(int)
         outgoing = collections.defaultdict(set)
-        info = {}
-        for i in range(len(recipes)):
-            info[recipes[i]] = ingredients[i]
-            
-            for child in ingredients[i]:
-                if child in rset:
-                    indegree[recipes[i]] += 1
-                    outgoing[child].add(recipes[i])
         
+        supplies = set(supplies)
+        rset = set(recipes)
+        info = {}
+        
+        for r, i in zip(recipes, ingredients):
+            info[r] = i
+            for j in i:
+                if j in rset:
+                    indegree[r] += 1
+                    outgoing[j].add(r)
         q = collections.deque()
         
-        for r in recipes:
+        for r in rset:
             if indegree[r] == 0:
                 q.append(r)
         
@@ -26,12 +25,15 @@ class Solution:
         
         while q:
             current = q.popleft()
-            if all(i in sset for i in info[current]):
+            if all([i in supplies for i in info[current]]):
+                supplies.add(current)
                 res.append(current)
-                sset.add(current)
                 
                 for o in outgoing[current]:
                     indegree[o] -= 1
                     if indegree[o] == 0:
                         q.append(o)
         return res
+                    
+        
+        
