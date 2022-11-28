f02fde50524951e440115d205037ff2b44907bdb
Yinan Yang <845280629abc@gmail.com>
2022-11-27 19:54:29 -0600

    Time: 250 ms (53.52%), Space: 15 MB (48.49%) - LeetHub

--- 1654-minimum-jumps-to-reach-home/1654-minimum-jumps-to-reach-home.py
+++ 1654-minimum-jumps-to-reach-home/1654-minimum-jumps-to-reach-home.py
@@ -1,40 +1,29 @@
-import collections
 class Solution:
     def minimumJumps(self, forbidden: List[int], a: int, b: int, x: int) -> int:
-        
         limit = 2000 + a + b
-        
-        q = collections.deque([(0, True)])
-        
         forbidden = set(forbidden)
-        
-        seen = set([(0, True)])
         
         jump = 0
         
+        q = collections.deque([(0, True)])
+        seen = set([(0, True)])
+        
         while q:
-            
             for _ in range(len(q)):
+                p, j = q.popleft()
                 
-                current, d = q.popleft()
-                
-                if current == x:
+                if p == x:
                     return jump
                 
-                if current + a <= limit and (current+a, True) not in seen and current+a not in forbidden:
-                    q.append((current+a, True))
-                    seen.add((current+a, True))
-                
-                if d:
-                    if current - b >= 0 and (current-b, False) not in seen and current-b not in forbidden:
-                        q.append((current-b, False))
-                        seen.add((current-b, False))
-            
+                new = p + a
+                if new <= limit and new not in forbidden and (new, True) not in seen:
+                        seen.add((new, True))
+                        q.append((new, True))
+                if not j:
+                    continue
+                new = p - b
+                if new >= 0 and new not in forbidden and (new, False) not in seen:
+                        seen.add((new, False))
+                        q.append((new, False))
             jump += 1
-        
         return -1
-                
-                
-                
-                
-            
