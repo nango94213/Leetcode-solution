870010c0d88a41303d1e7fb9da68a48cb881aec6
Yinan Yang <845280629abc@gmail.com>
2022-11-29 10:30:30 -0600

    Time: 6010 ms (7.59%), Space: 37.3 MB (6.67%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,4 +1,31 @@
-import heapq
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        return heapq.nlargest(k,nums)[-1]
+        
+        def partition(left, right, pivot):
+            nums[pivot], nums[right] = nums[pivot], nums[right]
+            
+            i = left
+            for j in range(left, right):
+                if nums[j] < nums[right]:
+                    nums[i], nums[j] = nums[j], nums[i]
+                    i += 1
+            nums[i], nums[right] = nums[right], nums[i]
+            return i
+        
+        def search(left, right):
+            if left == right:
+                return nums[left]
+            
+            pivot = random.randint(left, right)
+            i = partition(left, right, pivot)
+            
+            if i == len(nums) - k:
+                return nums[i]
+            
+            if i > len(nums) - k:
+                return search(left, i-1)
+            else:
+                return search(i+1, right)
+        
+        return search(0, len(nums)-1)
+        
