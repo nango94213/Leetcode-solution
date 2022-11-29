1287eed12acdd45a92577f221d64da34771cb4f7
Yinan Yang <845280629abc@gmail.com>
2022-11-29 10:23:09 -0600

    Time: 231 ms (51.96%), Space: 18.7 MB (47.63%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,44 +1,34 @@
-import heapq
+
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         count = Counter(nums)
         
         keys = list(count.keys())
         
-        def partition(left, right, pivot_index):
-        
-           pivot = keys[pivot_index]
-        
-           keys[pivot_index], keys[right] = keys[right], keys[pivot_index]
-        
-           i = left
-        
-           for j in range(left, right):
-                if count[keys[j]] < count[pivot]:
+        def partition(left, right, pivot):
+            p = keys[pivot]
+            keys[pivot], keys[right] = keys[right], keys[pivot]
+            
+            i = left
+            for j in range(left, right):
+                if count[keys[j]] < count[p]:
                     keys[i], keys[j] = keys[j], keys[i]
                     i += 1
+            keys[i], keys[right] = keys[right], keys[i]
+            return i
         
-           keys[i], keys[right] = keys[right], keys[i]
-        
-           return i
-    
         def search(left, right):
-        
             if left == right:
-                return keys[right:]
-           
-            pivot_index = random.randint(left, right)
-            i = partition(left, right, pivot_index)
+                return keys[left:]
+            
+            pivot = random.randint(left, right)
+            i = partition(left, right, pivot)
             
             if i == len(keys) - k:
                 return keys[i:]
-            
-            if i < len(keys) - k:
-                return search(i+1, right)
-            else:
+            if i > len(keys) - k:
                 return search(left, i-1)
+            else:
+                return search(i+1, right)
         
         return search(0, len(keys)-1)
-        
-        
-        
