947fcc78acb962a38ccf2815c9ed2b82896595cf
Yinan Yang <845280629abc@gmail.com>
2022-11-29 21:46:44 -0600

    Time: 102 ms (6.00%), Space: 14.1 MB (23.74%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,14 +1,14 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
+        
         res = []
+        
         def dfs(pool, path):
             
-            if len(path) == len(nums):
+            if not pool:
                 res.append(path)
-            
             for i in range(len(pool)):
-                dfs(pool[:i] + pool[i+1:], path+[pool[i]])
+                dfs(pool[:i]+pool[i+1:], path+[pool[i]])
         
         dfs(nums, [])
-        
         return res
