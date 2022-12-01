d0e18d891f148f256e08f62d94e56956f5acf0fb
Yinan Yang <845280629abc@gmail.com>
2022-12-01 10:22:33 -0600

    Time: 687 ms (20.19%), Space: 14.7 MB (83.13%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,25 +1,23 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        
         count = Counter(t)
-        k = len(t)
-        
-        left = 0
         res = ''
-        for right in range(len(s)):
-            if s[right] in count:
-                count[s[right]] -= 1
-                if count[s[right]] >= 0:
+        k = len(t)
+        left = 0
+        for i in range(len(s)):
+            if s[i] in count:
+                count[s[i]] -= 1
+                if count[s[i]] >= 0:
                     k -= 1
             
             while k == 0:
-                if not res or len(res) > right - left + 1:
-                    res = s[left:right+1]
+                if not res or len(res) > i - left + 1:
+                    res = s[left:i+1]
                 
                 if s[left] in count:
                     count[s[left]] += 1
                     if count[s[left]] > 0:
                         k += 1
                 left += 1
-        
         return res
+        
