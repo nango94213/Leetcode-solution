5e3f8f60d5b023e368ede5393fa4ad877dfb0373
Yinan Yang <845280629abc@gmail.com>
2022-12-01 00:05:59 -0600

    Time: 43 ms (87.75%), Space: 14.1 MB (49.63%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -1,17 +1,15 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        
         res = []
-        
         def dfs(pool, path):
-            
             res.append(path)
             
             for i in range(len(pool)):
-                if i > 0 and pool[i] == pool[i-1]:
+                if i != 0 and pool[i] == pool[i-1]:
                     continue
+                
                 dfs(pool[i+1:], path+[pool[i]])
         
         dfs(sorted(nums), [])
-        
         return res
+        
