900b316bafaf448c329c8f282661811ca1afd992
Yinan Yang <845280629abc@gmail.com>
2022-11-30 20:28:27 -0600

    Time: 126 ms (72.90%), Space: 15.6 MB (10.78%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,35 +1,32 @@
-import bisect
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-            if not nums:
-                return [-1, -1]
-            left = 0
-            right = len(nums) - 1
-            
-            while left < right:
-                mid = (left+right) // 2
-                if nums[mid] < target:
-                    left = mid + 1
-                else:
-                    right = mid
-            a = left
-            if nums[a] != target:
-                a = -1
-            
-            left = 0
-            right = len(nums) - 1
-            while left < right:
-                mid = (left+right) // 2
-                if nums[mid] <= target:
-                    left = mid + 1
-                else:
-                    right = mid
-            b = right 
-            if nums[b] != target:
-                if nums[b-1] == target:
-                    b -= 1
-                else:
-                    b = -1
-            
-            return [a, b]
+        if not nums:
+            return [-1, -1]
+        if len(nums) == 1:
+            return [0, 0] if nums[0] == target else [-1, -1]
+        left = 0
+        right = len(nums) - 1
         
+        while left < right:
+            mid = (left+right) // 2
+            if nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid
+        final1 = left
+        left = 0
+        right = len(nums) - 1
+       
+        while left <= right:
+            mid = (left+right) // 2
+    
+            if nums[mid] <= target:
+                left = mid + 1
+            else:
+                right = mid -1
+        final2 = left
+  
+        if nums[final1] != target:
+            return [-1, -1]
+        
+        return [final1, final2-1]
