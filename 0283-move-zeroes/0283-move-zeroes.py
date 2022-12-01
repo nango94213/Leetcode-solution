f830d18393b428348169cb830052004d43a18c1a
Yinan Yang <845280629abc@gmail.com>
2022-11-30 23:45:00 -0600

    Time: 483 ms (25.14%), Space: 15.6 MB (17.44%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -5,8 +5,9 @@ class Solution:
         """
         i = 0
         for j in range(len(nums)):
-            if nums[j] != 0:
-                nums[j], nums[i] = nums[i], nums[j]
+            if nums[j] == 0:
                 i += 1
+            else:
+                nums[j], nums[j-i] = nums[j-i], nums[j]
         return nums
         
