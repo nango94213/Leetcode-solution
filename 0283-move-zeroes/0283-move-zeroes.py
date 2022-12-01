ede1015cf565e9a99a9dca477a5346cd49273359
Yinan Yang <845280629abc@gmail.com>
2022-11-30 23:41:54 -0600

    Time: 460 ms (31.05%), Space: 15.6 MB (65.73%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,14 +3,10 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        
-        offset = 0
-        
-        for i in range(len(nums)):
-            if nums[i] == 0:
-                offset += 1
-                continue
-            
-            nums[i-offset], nums[i] = nums[i], nums[i-offset]
-        
+        i = 0
+        for j in range(len(nums)):
+            if nums[j] != 0:
+                nums[j], nums[i] = nums[i], nums[j]
+                i += 1
         return nums
+        
