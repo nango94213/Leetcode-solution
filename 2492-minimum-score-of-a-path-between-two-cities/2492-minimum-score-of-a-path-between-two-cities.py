16b45fa5190ba175301d943bfffd35dba599d4b8
Yinan Yang <845280629abc@gmail.com>
2022-12-04 00:09:44 -0600

    Time: 1655 ms (100.00%), Space: 74.2 MB (100.00%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
@@ -2,23 +2,21 @@ import collections
 
 class Solution:
     def minScore(self, n: int, roads: List[List[int]]) -> int:
-        graph = collections.defaultdict(set)
+        graph = collections.defaultdict(dict)
         for s, e, d in roads:
-            graph[s].add(e)
-            graph[e].add(s)
+            graph[s][e] = d
+            graph[e][s] = d
         
 
         q = collections.deque([1])
         seen = set([1])
+        res = float('inf')
         while q:
             current = q.popleft()
             for o in graph[current]:
                 if o not in seen:
                     seen.add(o)
                     q.append(o)
-        
-        res = float('inf')
-        for s, e, d in roads:
-            if s in seen and e in seen:
-                res = min(res, d)
+                res = min(res, graph[current][o])
+             
         return res
