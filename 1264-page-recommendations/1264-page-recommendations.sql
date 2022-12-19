764c479665c525a364db5a80aad52bd78edfb188
Yinan Yang <845280629abc@gmail.com>
2022-12-18 21:41:24 -0600

    Time: 577 ms (57.82%), Space: 0B (100.00%) - LeetHub

--- 1264-page-recommendations/1264-page-recommendations.sql
+++ 1264-page-recommendations/1264-page-recommendations.sql
@@ -1,10 +1,6 @@
 # Write your MySQL query statement below
 
-select distinct(tmd.recommended_page) from (select case
-when exists (select page_id target from Likes where user_id = 1) and l.page_id in (select page_id target from Likes where user_id = 1) then null
-else l.page_id
-end
-recommended_page from (select case 
+select distinct(l.page_id) recommended_page from (select case 
 when user1_id = 1 then user2_id
 when user2_id = 1 then user1_id
-end as real_id from Friendship) t join Likes l on t.real_id = l.user_id) tmd where tmd.recommended_page is not null
+end as real_id from Friendship) t join Likes l on t.real_id = l.user_id where l.page_id not in (select page_id from Likes where user_id = 1)
