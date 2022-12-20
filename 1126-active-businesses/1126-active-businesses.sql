00e44d3b4c07b4f359332ea5d234e75307ff51cb
Yinan Yang <845280629abc@gmail.com>
2022-12-19 20:19:08 -0600

    Time: 1153 ms (71.73%), Space: 0B (100.00%) - LeetHub

--- 1126-active-businesses/1126-active-businesses.sql
+++ 1126-active-businesses/1126-active-businesses.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
 
-select gg.business_id from (select e.business_id, sum(case when e.occurences > t.avg_act then 1 else 0 end) valid from Events e join (select event_type, avg(occurences) avg_act from Events group by event_type) t on e.event_type = t.event_type group by e.business_id) gg where gg.valid > 1
+select gg.business_id from (select e.business_id, sum(e.occurences > t.avg_act) valid from Events e join (select event_type, avg(occurences) avg_act from Events group by event_type) t on e.event_type = t.event_type group by e.business_id) gg where gg.valid > 1
