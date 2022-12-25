ae2dd329f9d884050fddb9d0a0fac1e24a800718
Yinan Yang <845280629abc@gmail.com>
2022-12-25 02:05:20 -0600

    Time: 306 ms (100.00%), Space: 14.9 MB (42.86%) - LeetHub

---
+++ 2516-take-k-of-each-character-from-left-and-right/2516-take-k-of-each-character-from-left-and-right.py
@@ -0,0 +1,18 @@
+class Solution:
+    def takeCharacters(self, s: str, k: int) -> int:
+        limits = {c: s.count(c) - k for c in 'abc'}
+        if any(x < 0 for x in limits.values()):
+            return -1
+        left = 0
+        count = Counter(s)
+        for key in count:
+            count[key] -=  k
+       
+        for right in range(len(s)):
+            count[s[right]] -= 1
+            
+            if count['a'] < 0 or count['b'] < 0 or count['c'] < 0:
+                count[s[left]] += 1
+                left += 1
+                
+        return len(s) - (right-left+1)
