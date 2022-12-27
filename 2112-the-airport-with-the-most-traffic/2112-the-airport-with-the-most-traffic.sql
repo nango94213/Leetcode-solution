a95d4461f0c07d75d9f24fe8895321b76225e2c6
Yinan Yang <845280629abc@gmail.com>
2022-12-27 15:06:22 -0600

    Time: 989 ms (7.29%), Space: 0B (100.00%) - LeetHub

--- 2112-the-airport-with-the-most-traffic/2112-the-airport-with-the-most-traffic.sql
+++ 2112-the-airport-with-the-most-traffic/2112-the-airport-with-the-most-traffic.sql
@@ -2,4 +2,4 @@
 
 with cte as(select departure_airport, flights_count from Flights union all select arrival_airport, flights_count from Flights)
 
-select departure_airport airport_id from (select *, rank() over(order by sum(flights_count) desc) ranking from cte group by departure_airport) t where t.ranking = 1
+select departure_airport airport_id from (select *, rank() over(order by total desc) ranking from (select departure_airport, sum(flights_count) total from cte group by departure_airport) t) g where g.ranking = 1
