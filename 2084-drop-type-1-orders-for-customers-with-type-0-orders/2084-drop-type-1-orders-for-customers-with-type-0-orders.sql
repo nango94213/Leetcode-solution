041f0fb791be953d054726128f21ebd7c1d67049
Yinan Yang <845280629abc@gmail.com>
2022-12-27 21:03:05 -0600

    Time: 771 ms (24.36%), Space: 0B (100.00%) - LeetHub

--- 2084-drop-type-1-orders-for-customers-with-type-0-orders/2084-drop-type-1-orders-for-customers-with-type-0-orders.sql
+++ 2084-drop-type-1-orders-for-customers-with-type-0-orders/2084-drop-type-1-orders-for-customers-with-type-0-orders.sql
@@ -2,4 +2,4 @@
 
 with cte as(select customer_id, min(order_type) order_type from Orders group by customer_id)
 
-select * from Orders where (customer_id, order_type) in (select * from cte)
+select * from Orders where (customer_id, order_type) in (select customer_id, min(order_type) order_type from Orders group by customer_id)
