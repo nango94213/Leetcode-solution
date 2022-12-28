b5b2c14f644e7777a9f5e710dba13baa54599d28
Yinan Yang <845280629abc@gmail.com>
2022-12-28 17:35:53 -0600

    Time: 1052 ms (5.19%), Space: 0B (100.00%) - LeetHub

--- 2298-tasks-count-in-the-weekend/2298-tasks-count-in-the-weekend.sql
+++ 2298-tasks-count-in-the-weekend/2298-tasks-count-in-the-weekend.sql
@@ -1,4 +1,3 @@
 # Write your MySQL query statement below
-with cte as(select *, case when weekday(submit_date) < 5 then 1 else null end weekend_cnt, case when weekday(submit_date) >= 5 then 1 else null end working_cnt from Tasks)
 
-select count(weekend_cnt) weekend_cnt, count(working_cnt) working_cnt from cte
+select sum(weekday(submit_date)<5) weekend_cnt, sum(weekday(submit_date)>=5) working_cnt from Tasks
