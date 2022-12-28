abb2b180708b22016c9ac8fe68ed4d4fb986bf1a
Yinan Yang <845280629abc@gmail.com>
2022-12-28 17:38:41 -0600

    Time: 384 ms (92.35%), Space: 0B (100.00%) - LeetHub

--- 2298-tasks-count-in-the-weekend/2298-tasks-count-in-the-weekend.sql
+++ 2298-tasks-count-in-the-weekend/2298-tasks-count-in-the-weekend.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
 
-select sum(weekday(submit_date)<5) weekend_cnt, sum(weekday(submit_date)>=5) working_cnt from Tasks
+select count(case when weekday(submit_date)<5 then 1 else null end) weekend_cnt, count(case when weekday(submit_date)>=5 then 1 else null end) working_cnt from Tasks
