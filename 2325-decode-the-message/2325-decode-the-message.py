2662e9ad58b9801a85f00a1939f897169f31279e
Yinan Yang <845280629abc@gmail.com>
2022-12-31 00:33:41 -0600

    Time: 29 ms (97.86%), Space: 13.9 MB (38.81%) - LeetHub

--- 2325-decode-the-message/2325-decode-the-message.py
+++ 2325-decode-the-message/2325-decode-the-message.py
@@ -3,13 +3,12 @@ class Solution:
         default = 'abcdefghijklmnopqrstuvwxyz'
         j = 0
         dic = {}
-        for i in range(len(key)):
-            if len(dic) == 26:
-                break
-            if key[i] != ' ' and key[i] not in dic:
-                dic[key[i]] = default[j]
-                j += 1
         dic[' '] = ' '
+        
+        for k in key:
+            if k not in dic:
+                dic[k] = default[j]
+                j += 1
         res = ''
         for c in message:
             res += dic[c]
