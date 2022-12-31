082faa014af92acdfb17bb96618b2a65179a7ac9
Yinan Yang <845280629abc@gmail.com>
2022-12-30 18:47:47 -0600

    Time: 1555 ms (57.13%), Space: 0B (100.00%) - LeetHub

--- 1783-grand-slam-titles/1783-grand-slam-titles.sql
+++ 1783-grand-slam-titles/1783-grand-slam-titles.sql
@@ -2,6 +2,6 @@
 
 with cte as(select Wimbledon counting from Championships  union all select Fr_open from Championships union all select US_open from Championships Union all select Au_open from Championships),
 
-cte2 as(select player_id, player_name, count(player_id) grand_slams_count from Players p join cte c on p.player_id = c.counting group by player_id order by grand_slams_count asc)
+cte2 as(select player_id, player_name, count(player_id) grand_slams_count from Players p join cte c on p.player_id = c.counting group by player_name order by grand_slams_count asc)
 
 select * from cte2
