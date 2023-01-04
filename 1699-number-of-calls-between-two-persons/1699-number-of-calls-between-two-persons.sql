d11569e3e965eef44efc409276ec9838f9b24f75
Yinan Yang <845280629abc@gmail.com>
2023-01-03 23:34:59 -0600

    Time: 750 ms (69.39%), Space: 0B (100.00%) - LeetHub

--- 1699-number-of-calls-between-two-persons/1699-number-of-calls-between-two-persons.sql
+++ 1699-number-of-calls-between-two-persons/1699-number-of-calls-between-two-persons.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 
 
-select case when from_id < to_id then from_id else to_id end person1, case when from_id < to_id then to_id else from_id end person2, count(duration) call_count, sum(duration) total_duration from Calls group by person1, person2
+select if (from_id < to_id, from_id, to_id) person1, if (from_id < to_id, to_id, from_id) person2, count(duration) call_count, sum(duration) total_duration from Calls group by person1, person2
