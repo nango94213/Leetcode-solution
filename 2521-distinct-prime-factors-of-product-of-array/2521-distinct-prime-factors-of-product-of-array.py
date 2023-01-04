f112744c31432605cf05ce02c28b9f34ef157416
Yinan Yang <845280629abc@gmail.com>
2023-01-04 11:11:02 -0600

    Time: 1465 ms (24.02%), Space: 15.3 MB (69.76%) - LeetHub

--- 2521-distinct-prime-factors-of-product-of-array/2521-distinct-prime-factors-of-product-of-array.py
+++ 2521-distinct-prime-factors-of-product-of-array/2521-distinct-prime-factors-of-product-of-array.py
@@ -1,16 +1,15 @@
 class Solution:
     def distinctPrimeFactors(self, nums: List[int]) -> int:
-     
         res = set()
         for n in nums:
-            c = 2
-          
-            while n > 1:
+            for i in range(2, n+1):
+                if n % i == 0:
+                    res.add(i)
+                    
+                    while n % i == 0:
+                        n =  n // i
                 
-                if n % c == 0:
-                    n = n // c
-                    res.add(c)
-                else:
-                    c = c + 1
+                if n == 1:
+                    break
 
         return len(res)
