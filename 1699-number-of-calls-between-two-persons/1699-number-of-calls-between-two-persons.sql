fe75ec13546e322597af194b599de11b98938d97
Yinan Yang <845280629abc@gmail.com>
2023-01-03 23:33:49 -0600

    Time: 1235 ms (8.72%), Space: 0B (100.00%) - LeetHub

--- 1699-number-of-calls-between-two-persons/1699-number-of-calls-between-two-persons.sql
+++ 1699-number-of-calls-between-two-persons/1699-number-of-calls-between-two-persons.sql
@@ -1,7 +1,4 @@
 # Write your MySQL query statement below
 
-with cte as(select * from Calls union all select to_id, from_id, duration from Calls),
 
-cte2 as(select * from cte where from_id < to_id)
-
-select from_id person1, to_id person2, count(duration) call_count, sum(duration) total_duration from cte2 group by from_id, to_id
+select case when from_id < to_id then from_id else to_id end person1, case when from_id < to_id then to_id else from_id end person2, count(duration) call_count, sum(duration) total_duration from Calls group by person1, person2
