861a83ebf0ecf94a231839774ba6b5a489c3d06e
Yinan Yang <845280629abc@gmail.com>
2023-01-05 22:56:48 -0600

    Time: 2454 ms (58.71%), Space: 0B (100.00%) - LeetHub

--- 1596-the-most-frequently-ordered-products-for-each-customer/1596-the-most-frequently-ordered-products-for-each-customer.sql
+++ 1596-the-most-frequently-ordered-products-for-each-customer/1596-the-most-frequently-ordered-products-for-each-customer.sql
@@ -1,6 +1,4 @@
 # Write your MySQL query statement below
-with cte as(select c.customer_id, p.product_id, count(p.product_id) total, p.product_name from Customers c join Orders o on c.customer_id = o.customer_id join Products p on o.product_id = p.product_id group by c.customer_id, p.product_id),
+with cte as(select c.customer_id, p.product_id, rank() over(partition by customer_id order by count(*) desc) rk, p.product_name from Customers c join Orders o on c.customer_id = o.customer_id join Products p on o.product_id = p.product_id group by c.customer_id, p.product_id)
 
-cte2 as(select *, rank() over(partition by customer_id order by total desc) rk from cte)
-
-select customer_id, product_id, product_name from cte2 where rk = 1
+select customer_id, product_id, product_name from cte where rk = 1
