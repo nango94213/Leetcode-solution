170e96d7a1092c71cc1977ef456304b9f6381060
Yinan Yang <845280629abc@gmail.com>
2023-01-05 22:32:12 -0600

    Time: 2565 ms (5.00%), Space: 0B (100.00%) - LeetHub

--- 1532-the-most-recent-three-orders/1532-the-most-recent-three-orders.sql
+++ 1532-the-most-recent-three-orders/1532-the-most-recent-three-orders.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 with cte as(select c.customer_id, c.name, o.order_id, o.order_date, rank() over(partition by c.customer_id order by order_date desc) rk from Customers c join Orders o on c.customer_id = o.customer_id order by c.name asc, c.customer_id asc, o.order_date desc)
 
-select name customer_name, customer_id, order_id, order_date from cte where rk in (1, 2, 3) 
+select name customer_name, customer_id, order_id, order_date from cte where rk <= 3
