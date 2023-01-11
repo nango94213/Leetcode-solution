a4044f3ba6d7c58ebdcd27b67576b0c0caac3220
Yinan Yang <845280629abc@gmail.com>
2023-01-11 16:27:52 -0600

    Time: 908 ms (58.15%), Space: 0B (100.00%) - LeetHub

--- 1308-running-total-for-different-genders/1308-running-total-for-different-genders.sql
+++ 1308-running-total-for-different-genders/1308-running-total-for-different-genders.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
 
-select gender, day, sum(score_points) over(partition by gender order by gender asc, day asc) total from Scores
+select gender, day, sum(score_points) over(partition by gender order by day asc) total from Scores
