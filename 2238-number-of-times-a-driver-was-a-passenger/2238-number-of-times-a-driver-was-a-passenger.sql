30f23699c3e6ff90f0ef6da1157dc1bfdf2cfeab
Yinan Yang <845280629abc@gmail.com>
2023-01-17 23:30:09 -0600

    Time: 1801 ms (5.13%), Space: 0B (100.00%) - LeetHub

--- 2238-number-of-times-a-driver-was-a-passenger/2238-number-of-times-a-driver-was-a-passenger.sql
+++ 2238-number-of-times-a-driver-was-a-passenger/2238-number-of-times-a-driver-was-a-passenger.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
 
 with cte as(select distinct(driver_id) dd from Rides)
-
 select dd driver_id, count(passenger_id) cnt from cte left join Rides on dd = passenger_id group by dd
+
