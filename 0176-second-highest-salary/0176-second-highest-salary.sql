f8a7d32928b82756d1cc7deb73a4fd3d96efccd5
Yinan Yang <845280629abc@gmail.com>
2023-01-19 11:28:36 -0600

    Time: 488 ms (13.04%), Space: 0B (100.00%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,3 +1,4 @@
 # Write your MySQL query statement below
 
-select max(salary) SecondHighestSalary from (select *, dense_rank() over(order by salary desc) rk from Employee) t where rk = 2
+select IFNULL((select max(salary) SecondHighestSalary from (select *, dense_rank() over(order by salary desc) rk from Employee) t where rk = 2 limit 1), NULL) SecondHighestSalary
+#select max(salary) SecondHighestSalary from (select *, dense_rank() over(order by salary desc) rk from Employee) t where rk = 2
