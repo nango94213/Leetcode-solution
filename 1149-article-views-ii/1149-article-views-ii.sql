729a6e8ba784181c5b550c175dc1edfbb06e0cc8
Yinan Yang <845280629abc@gmail.com>
2023-01-19 02:08:45 -0600

    Time: 635 ms (47.74%), Space: 0B (100.00%) - LeetHub

--- 1149-article-views-ii/1149-article-views-ii.sql
+++ 1149-article-views-ii/1149-article-views-ii.sql
@@ -1,8 +1,4 @@
 # Write your MySQL query statement below
-with cte as(select article_id, viewer_id, view_date from Views),
 
-cte2 as(select * from cte union select * from cte),
 
-cte3 as(select count(*) ct, viewer_id from cte2 group by view_date, viewer_id)
-
-select distinct(viewer_id) id from cte3 where ct > 1 order by id asc
+select distinct viewer_id id from Views group by view_date, viewer_id having count(distinct article_id) > 1 order by id asc
