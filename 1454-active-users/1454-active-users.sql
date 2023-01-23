477f62314579b3a83b11649af9f15883fd222958
Yinan Yang <845280629abc@gmail.com>
2023-01-22 23:15:59 -0600

    Time: 2408 ms (32.64%), Space: 0B (100.00%) - LeetHub

---
+++ 1454-active-users/1454-active-users.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+
+with cte as(select * from Logins union select * from Logins),
+
+cte2 as(select *, dense_rank() over(partition by id order by login_date asc) rn from cte),
+
+cte3 as(select id, DATEDIFF(login_date, CURDATE()) - cast(rn as signed) diff, count(*) total from cte2 group by id, diff order by id, login_date asc)
+
+select distinct a.id, a.name from Accounts a join cte3 b on a.id = b.id where total >= 5 order by id asc
+
+
+
+
