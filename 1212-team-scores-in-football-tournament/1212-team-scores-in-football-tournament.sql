ec6b1cbcc63b4cf9300e39598c481e092bfc51a5
Yinan Yang <845280629abc@gmail.com>
2023-01-23 11:57:32 -0600

    Time: 1243 ms (31.40%), Space: 0B (100.00%) - LeetHub

--- 1212-team-scores-in-football-tournament/1212-team-scores-in-football-tournament.sql
+++ 1212-team-scores-in-football-tournament/1212-team-scores-in-football-tournament.sql
@@ -2,4 +2,4 @@
 
 with cte as (select host_team, guest_team, host_goals, guest_goals from Matches union all select guest_team, host_team, guest_goals, host_goals from Matches)
 
-select a.team_id, a.team_name, ifnull(gg.num_points, 0) num_points from Teams a left join (select host_team, sum(case when host_goals > guest_goals then 3 when host_goals = guest_goals then 1 else 0 end) num_points from cte group by host_team) gg on a.team_id = gg.host_team order by num_points desc, team_id asc
+select a.team_id, a.team_name, if(gg.num_points is null, 0, gg.num_points) num_points from Teams a left join (select host_team, sum(case when host_goals > guest_goals then 3 when host_goals = guest_goals then 1 else 0 end) num_points from cte group by host_team) gg on a.team_id = gg.host_team order by num_points desc, team_id asc
