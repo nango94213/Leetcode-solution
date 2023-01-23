ace0c73b23f9f069ea866507644905e671710add
Yinan Yang <845280629abc@gmail.com>
2023-01-22 23:18:53 -0600

    Time: 3210 ms (8.00%), Space: 0B (100.00%) - LeetHub

--- 1454-active-users/1454-active-users.sql
+++ 1454-active-users/1454-active-users.sql
@@ -1,8 +1,8 @@
 # Write your MySQL query statement below
 
-with cte as(select * from Logins),
+with cte as(select distinct * from Logins),
 
-cte2 as(select distinct *, dense_rank() over(partition by id order by login_date asc) rn from cte),
+cte2 as(select *, dense_rank() over(partition by id order by login_date asc) rn from cte),
 
 cte3 as(select id, DATEDIFF(login_date, CURDATE()) - cast(rn as signed) diff, count(*) total from cte2 group by id, diff order by id, login_date asc)
 
