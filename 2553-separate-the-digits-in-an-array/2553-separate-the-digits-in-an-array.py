449524d0e3b454e5d571e1d29f9a845799c51c37
Yinan Yang <845280629abc@gmail.com>
2023-02-05 18:22:45 -0600

    Time: 66 ms (100.00%), Space: 14.5 MB (45.45%) - LeetHub

--- 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.py
+++ 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.py
@@ -3,11 +3,6 @@ class Solution:
         res = []
         
         for n in nums:
-            tmp = []
-            while n:
-                tmp.append(n%10)
-                n //= 10
-            for i in range(len(tmp)-1, -1, -1):
-                res.append(tmp[i])
+            res += list(str(n))
         
-        return res
+        return map(int, res)
