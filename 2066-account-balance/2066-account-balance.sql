0f9ff9dfa3145515cd0ceb5c9b0915c6fb41dcfb
Yinan Yang <845280629abc@gmail.com>
2023-02-05 18:10:19 -0600

    Time: 775 ms (51.12%), Space: 0B (100.00%) - LeetHub

--- 2066-account-balance/2066-account-balance.sql
+++ 2066-account-balance/2066-account-balance.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
 
-select account_id, day, sum(if(type='Deposit', amount, -amount)) over(partition by account_id order by day asc) balance from Transactions order by account_id asc, day asc
+select account_id, day, sum(if(type='Deposit', amount, -amount)) over(partition by account_id order by day asc) balance from Transactions
