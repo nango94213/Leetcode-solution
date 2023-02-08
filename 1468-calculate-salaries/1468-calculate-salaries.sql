73480569e0e60b1880854fe343e6a6f2b16828b3
Yinan Yang <845280629abc@gmail.com>
2023-02-07 18:33:04 -0600

    Time: 1146 ms (43.43%), Space: 0B (100.00%) - LeetHub

--- 1468-calculate-salaries/1468-calculate-salaries.sql
+++ 1468-calculate-salaries/1468-calculate-salaries.sql
@@ -1,4 +1,2 @@
 # Write your MySQL query statement below
-with cte as(select *, max(salary) over(partition by company_id) best from Salaries)
-
-select company_id, employee_id, employee_name, case when best < 1000 then round(salary, 0) when best <= 10000 then round(salary * 0.76, 0) else round(salary * 0.51, 0) end salary from cte
+select company_id, employee_id, employee_name, round(case when ms < 1000 then salary when ms <= 10000 then salary*(1-24/100) else salary*(1-49/100) end, 0) salary from (select *, max(salary) over(partition by company_id) ms from Salaries) tmp
