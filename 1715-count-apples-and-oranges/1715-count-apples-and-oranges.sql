06441a93f1a6f2f9222cf778280e75dddcf59207
Yinan Yang <845280629abc@gmail.com>
2023-02-10 23:01:33 -0600

    Time: 1763 ms (45.48%), Space: 0B (100.00%) - LeetHub

--- 1715-count-apples-and-oranges/1715-count-apples-and-oranges.sql
+++ 1715-count-apples-and-oranges/1715-count-apples-and-oranges.sql
@@ -1,2 +1,5 @@
 # Write your MySQL query statement below
-select sum(a.apple_count+ifnull(b.apple_count, 0)) apple_count, sum(a.orange_count+ifnull(b.orange_count, 0)) orange_count from Boxes a left join Chests b on a.chest_id = b.chest_id
+
+with cte as(select b.apple_count a , b.orange_count b , c.apple_count c , c.orange_count d from Boxes b left join Chests c on b.chest_id = c.chest_id)
+
+select sum(a) apple_count, sum(b) orange_count from (select a, b from cte union all select c, d from cte) gg
