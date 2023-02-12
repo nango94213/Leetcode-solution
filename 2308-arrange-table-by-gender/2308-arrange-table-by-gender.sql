6166867af22ce90860501a435872d0a602e08428
Yinan Yang <845280629abc@gmail.com>
2023-02-12 01:37:14 -0600

    Time: 889 ms (45.74%), Space: 0B (100.00%) - LeetHub

--- 2308-arrange-table-by-gender/2308-arrange-table-by-gender.sql
+++ 2308-arrange-table-by-gender/2308-arrange-table-by-gender.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
 
-with cte as(select *, rank() over(partition by gender order by user_id asc) ranking from Genders)
+with cte as(select *, rank() over(partition by gender order by user_id) rk from Genders)
 
-select user_id, gender from cte order by ranking asc, length(gender) desc
+select user_id, gender from cte order by rk, length(gender) desc
