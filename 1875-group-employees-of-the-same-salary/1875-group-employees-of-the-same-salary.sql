ac37d3fb006c47f3d7b18775ec2a95a3961e4a72
Yinan Yang <845280629abc@gmail.com>
2023-02-12 13:14:05 -0600

    Time: 1168 ms (29.42%), Space: 0B (100.00%) - LeetHub

--- 1875-group-employees-of-the-same-salary/1875-group-employees-of-the-same-salary.sql
+++ 1875-group-employees-of-the-same-salary/1875-group-employees-of-the-same-salary.sql
@@ -2,4 +2,4 @@
 
 with cte as(select salary from Employees group by salary having count(name) >= 2)
 
-select a.employee_id, a.name, a.salary, dense_rank() over(order by salary asc) team_id from Employees a join cte b on a.salary = b.salary
+select a.employee_id, a.name, a.salary, dense_rank() over(order by salary asc) team_id from Employees a join cte b on a.salary = b.salary order by team_id, employee_id
