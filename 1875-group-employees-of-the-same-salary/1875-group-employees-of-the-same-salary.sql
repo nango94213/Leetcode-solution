81048e85a66e214ddc2aa0f4f3a77587e9c87f6b
Yinan Yang <845280629abc@gmail.com>
2023-02-12 13:10:18 -0600

    Time: 1292 ms (14.26%), Space: 0B (100.00%) - LeetHub

--- 1875-group-employees-of-the-same-salary/1875-group-employees-of-the-same-salary.sql
+++ 1875-group-employees-of-the-same-salary/1875-group-employees-of-the-same-salary.sql
@@ -1,6 +1,5 @@
 # Write your MySQL query statement below
 
-with cte as(select *, count(salary) over(partition by salary) ct from Employees)
+with cte as(select salary from Employees group by salary having count(name) >= 2)
 
-
-select employee_id, name, salary, dense_rank() over(order by salary) team_id from cte where ct >= 2
+select a.employee_id, a.name, a.salary, dense_rank() over(order by salary asc) team_id from Employees a join cte b on a.salary = b.salary
