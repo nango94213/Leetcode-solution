a6bac8bdd6aa4c6af8fed66e6ca2effff32c4a9b
Yinan Yang <845280629abc@gmail.com>
2023-02-12 14:26:50 -0600

    Time: 675 ms (60.41%), Space: 0B (100.00%) - LeetHub

--- 1613-find-the-missing-ids/1613-find-the-missing-ids.sql
+++ 1613-find-the-missing-ids/1613-find-the-missing-ids.sql
@@ -1,7 +1,4 @@
 # Write your MySQL query statement below
-WITH RECURSIVE seq AS (
-    SELECT 1 AS value UNION ALL SELECT value + 1 FROM seq WHERE value <= 100
-    ),
-cte as(select max(customer_id) mx from Customers)
-
-SELECT value ids FROM seq, cte where value not in (select customer_id from Customers) and value <= mx
+WITH recursive numbers AS (SELECT 1 AS num UNION ALL SELECT num + 1 FROM numbers WHERE num < (select max(customer_id) from Customers))
+  
+select num ids from numbers where num not in (select customer_id from Customers)
