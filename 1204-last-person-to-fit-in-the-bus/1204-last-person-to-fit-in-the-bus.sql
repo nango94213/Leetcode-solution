28e0a3fcf99bbd62d6663f5c737e1e887ef1008a
Yinan Yang <845280629abc@gmail.com>
2023-02-13 22:06:33 -0600

    Time: 1415 ms (59.01%), Space: 0B (100.00%) - LeetHub

--- 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -1,6 +1,4 @@
 # Write your MySQL query statement below
-with cte as(select *, sum(weight) over(order by turn) total from Queue),
+with cte as(select *, sum(weight) over(order by turn) total from Queue)
 
-cte2 as(select *, lead(total) over() ld from cte)
-
-select person_name from cte2 where total <= 1000 and (ld is null or ld >  1000)
+select person_name from cte where total <= 1000 order by total desc limit 1
