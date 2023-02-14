9e431c800f502a98ea59fa924571647ae98c5fab
Yinan Yang <845280629abc@gmail.com>
2023-02-13 18:52:03 -0600

    Time: 1461 ms (25.40%), Space: 0B (100.00%) - LeetHub

--- 1355-activity-participants/1355-activity-participants.sql
+++ 1355-activity-participants/1355-activity-participants.sql
@@ -1,5 +1,4 @@
 # Write your MySQL query statement below
-with cte as(select activity, rank() over(order by count(*) desc) rk from Friends group by activity)
+with cte as(select activity, rank() over(order by count(*) asc) r1, rank() over(order by count(*) desc) r2 from Friends group by activity)
 
-
-select activity from cte, (select max(rk) total from cte) gg where rk != 1 and rk != total
+select activity from cte where r1 != 1 and r2 != 1
