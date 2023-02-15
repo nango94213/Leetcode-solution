8ce8a3524c18f56802fba6688362fbf82aaaa6ed
Yinan Yang <845280629abc@gmail.com>
2023-02-14 22:11:28 -0600

    Time: 1216 ms (15.04%), Space: 0B (100.00%) - LeetHub

--- 1951-all-the-pairs-with-the-maximum-number-of-common-followers/1951-all-the-pairs-with-the-maximum-number-of-common-followers.sql
+++ 1951-all-the-pairs-with-the-maximum-number-of-common-followers/1951-all-the-pairs-with-the-maximum-number-of-common-followers.sql
@@ -1,5 +1,3 @@
 # Write your MySQL query statement below
 
-with cte as(select a.user_id user1_id, b.user_id user2_id, count(*) ct from Relations a join Relations b on a.user_id < b.user_id and a.follower_id = b.follower_id group by a.user_id, b.user_id)
-
-select user1_id, user2_id from (select *, rank() over(order by ct desc) rk from cte) gg where gg.rk = 1
+select ad user1_id, bd user2_id from (select a.user_id ad, b.user_id bd, rank() over(order by count(*) desc) total from Relations a join Relations b on a.follower_id = b.follower_id and a.user_id != b.user_id where a.user_id < b.user_id group by ad, bd) t where total = 1
