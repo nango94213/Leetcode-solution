dbc7ad2f01673fab7b5b90398ae2ce1f4671f6f0
Yinan Yang <845280629abc@gmail.com>
2023-02-16 21:48:02 -0600

    Time: 1143 ms (16.01%), Space: 0B (100.00%) - LeetHub

--- 1747-leetflex-banned-accounts/1747-leetflex-banned-accounts.sql
+++ 1747-leetflex-banned-accounts/1747-leetflex-banned-accounts.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
 
-select distinct(a.account_id) from LogInfo a join LogInfo b on a.account_id = b.account_id and a.ip_address != b.ip_address where a.login between b.login and b.logout
+select distinct a.account_id from LogInfo a join LogInfo b on a.account_id = b.account_id where a.ip_address != b.ip_address and a.login between b.login and b.logout
