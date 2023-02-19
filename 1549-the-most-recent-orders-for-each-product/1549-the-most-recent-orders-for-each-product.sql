f3ff047c8d24bedfcd8f4a245f62bf71f2ed6bba
Yinan Yang <845280629abc@gmail.com>
2023-02-19 00:06:10 -0600

    Time: 3890 ms (61.39%), Space: 0B (100.00%) - LeetHub

--- 1549-the-most-recent-orders-for-each-product/1549-the-most-recent-orders-for-each-product.sql
+++ 1549-the-most-recent-orders-for-each-product/1549-the-most-recent-orders-for-each-product.sql
@@ -1,5 +1,3 @@
 # Write your MySQL query statement below
 
-with cte as(select p.product_name, p.product_id, o.order_id, o.order_date, rank() over(partition by p.product_id order by o.order_date desc) rk from Products p join Orders o on p.product_id = o.product_id)
-
-select product_name, product_id, order_id, order_date from cte where rk = 1 order by product_name asc, product_id asc, order_id asc
+select product_name, product_id, order_id, order_date from (select b.product_name, b.product_id, a.order_id, a.order_date, rank() over(partition by b.product_id order by a.order_date desc) rk from Orders a join Products b on a.product_id = b.product_id) t where t.rk = 1 order by product_name, product_id, order_id
