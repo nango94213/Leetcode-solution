8dbe1788466098b5ad0f0aed09af340d2c95cf4e
Yinan Yang <845280629abc@gmail.com>
2023-03-01 22:29:09 -0600

    Time: 1278 ms (14.13%), Space: 0B (100.00%) - LeetHub

--- 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
 
-select DATE_FORMAT(trans_date, '%Y-%m') month, country, count(*) trans_count, sum(state='approved') approved_count, sum(amount) trans_total_amount, sum(case when state = 'approved' then amount else 0 end) approved_total_amount from Transactions group by month, country
+select DATE_FORMAT(trans_date, '%Y-%m') month, country, count(*) trans_count, sum(state='approved') approved_count, sum(amount) trans_total_amount, sum(if(state='approved', amount, 0)) approved_total_amount from Transactions group by month, country
