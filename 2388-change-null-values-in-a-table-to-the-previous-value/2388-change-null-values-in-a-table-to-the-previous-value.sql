9be38a1f9ab12504e92c3df406107165a2b39aee
Yinan Yang <845280629abc@gmail.com>
2023-03-04 01:04:34 -0600

    Time: 1537 ms (5.01%), Space: 0B (100.00%) - LeetHub

--- 2388-change-null-values-in-a-table-to-the-previous-value/2388-change-null-values-in-a-table-to-the-previous-value.sql
+++ 2388-change-null-values-in-a-table-to-the-previous-value/2388-change-null-values-in-a-table-to-the-previous-value.sql
@@ -1,6 +1,7 @@
 # Write your MySQL query statement below
-with cte as (select *, row_number() over() rn from CoffeeShop),
 
-cte2 as(select *, count(drink) over (order by rn) cnt from cte)
+with cte as(select *, row_number() over() rn from CoffeeShop),
 
-select id, first_value(drink) over(partition by cnt) drink from cte2
+cte2 as(select *, count(drink) over(order by rn) gg from cte)
+
+select id, first_value(drink)  over(partition by gg) drink from cte2
