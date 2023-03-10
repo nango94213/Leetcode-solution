4ab7ae676cee3d086d3a37266663d0a4d6bf8c18
Yinan Yang <845280629abc@gmail.com>
2023-03-10 15:39:42 -0600

    Time: 398 ms (89.10%), Space: 0B (100.00%) - LeetHub

--- 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -1,7 +1,3 @@
 # Write your MySQL query statement below
 
-with cte as(select requester_id id from RequestAccepted union all select accepter_id from RequestAccepted),
-
-cte2 as(select id, count(id) total from cte group by id)
-
-select t.id, t.total num from (select *, rank() over(order by total desc) ranking from cte2) t where t.ranking = 1
+select id, num from (select requester_id id, count(*) num, rank() over(order by count(*) desc) rk from (select requester_id, accepter_id from RequestAccepted union all select accepter_id, requester_id from RequestAccepted) a group by id) b where b.rk = 1
