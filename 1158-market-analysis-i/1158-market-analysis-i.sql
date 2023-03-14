c2c9f0adcda94d4391b231193befe0b67c4ded29
Yinan Yang <845280629abc@gmail.com>
2023-03-13 21:54:01 -0500

    Time: 2421 ms (41.28%), Space: 0B (100.00%) - LeetHub

--- 1158-market-analysis-i/1158-market-analysis-i.sql
+++ 1158-market-analysis-i/1158-market-analysis-i.sql
@@ -1,7 +1,3 @@
 # Write your MySQL query statement below
 
- select t.user_id buyer_id, t.join_date, sum(t.info) orders_in_2019 from (select u.user_id, u.join_date,
-  CASE
-    WHEN year(o.order_date) = 2019 THEN 1
-    ELSE 0
-  END info from Users u left join Orders o on u.user_id = o.buyer_id) t group by t.user_id order by t.user_id asc
+select a.user_id buyer_id, a.join_date, count(b.order_id) orders_in_2019 from Users a left join (select * from Orders where year(order_date)=2019) b on a.user_id = b.buyer_id group by a.user_id
