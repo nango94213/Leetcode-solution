e50910dc16f6eef33a5fbb30078dbc467dec2da2
Yinan Yang <845280629abc@gmail.com>
2023-03-16 16:09:53 -0500

    Time: 2364 ms (29.26%), Space: 0B (100.00%) - LeetHub

--- 1341-movie-rating/1341-movie-rating.sql
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -1,9 +1,7 @@
 # Write your MySQL query statement below
 
-with cte as(select u.user_id, u.name, rank() over(order by count(*) desc, u.name asc) rk from Users u join MovieRating m on u.user_id = m.user_id group by u.user_id),
+with cte as(select name from (select a.user_id, b.name, rank() over(order by count(*) desc, b.name) rk from MovieRating a join Users b on a.user_id = b.user_id group by a.user_id) tmp where rk = 1),
 
-cte2 as(select m.title, rank() over(order by avg(r.rating) desc, m.title asc) rk from Movies m join MovieRating r on m.movie_id = r.movie_id where r.created_at between '2020-02-01' and '2020-02-28' group by m.title)
+cte2 as(select title from (select a.movie_id, b.title, rank() over(order by avg(rating) desc, b.title) rk from MovieRating a join Movies b on a.movie_id = b.movie_id where created_at between '2020-02-01' and '2020-02-28' group by a.movie_id) tmp where rk = 1)
 
-
-
-select name results from cte where rk = 1 union select title from cte2 where rk = 1
+select name results from cte union select title from cte2 
