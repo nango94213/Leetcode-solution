2bdaf8aeea0d70d1cf07de609bc5cee006680834
Yinan Yang <845280629abc@gmail.com>
2023-04-12 22:02:19 -0600

    Time: 789 ms (85.52%), Space: 26 MB (70.17%) - LeetHub

---
+++ 2614-prime-in-diagonal/2614-prime-in-diagonal.py
@@ -0,0 +1,25 @@
+class Solution:
+    def diagonalPrime(self, nums: List[List[int]]) -> int:
+        def is_prime(n):
+            if n <= 1:
+                return False
+            if n <= 3:
+                return True
+            if n % 2 == 0 or n % 3 == 0:
+                return False
+
+            i = 5
+            while i * i <= n:
+                if n % i == 0 or n % (i + 2) == 0:
+                        return False
+                i += 6
+
+            return True
+        res = 0
+        n = len(nums) - 1
+        for i in range(len(nums)):
+            if is_prime(nums[i][i]):
+                res = max(res, nums[i][i])
+            if is_prime(nums[n-i][i]):
+                res = max(res, nums[n-i][i])
+        return res
