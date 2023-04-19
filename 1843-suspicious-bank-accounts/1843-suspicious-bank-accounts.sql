87f0a12a50c5dc4f86eb76df0eb3147a27eb870b
Yinan Yang <845280629abc@gmail.com>
2023-04-19 16:27:26 -0500

    Time: 1612 ms (96.71%), Space: 0B (100.00%) - LeetHub

--- 1843-suspicious-bank-accounts/1843-suspicious-bank-accounts.sql
+++ 1843-suspicious-bank-accounts/1843-suspicious-bank-accounts.sql
@@ -1,7 +1,8 @@
 # Write your MySQL query statement below
 
-with cte as(select transaction_id, account_id, year(day) y, month(day) m, sum(amount) income from Transactions where type = "Creditor" group by account_id, y, m order by account_id, y, m),
+with cte as(select account_id, year(day) y, month(day) m, sum(if(type="Creditor", amount, 0)) total from Transactions group by account_id, y, m),
 
-cte2 as(select a.transaction_id, a.account_id, a.y, a.m m1, a.income i1, b.m m2, b.income i2 from cte a join cte b on a.account_id = b.account_id and a.y = b.y and a.m = b.m-1)
+cte2 as(select a.account_id, a.m, a.total, b.m m1, b.total t1 from cte a join cte b on a.account_id = b.account_id and a.y = b.y and a.m = b.m+1)
 
-select distinct a.account_id from Accounts a join cte2 b on a.account_id = b.account_id where i1 > max_income and i2 > max_income order by transaction_id asc
+#select * from cte2
+select distinct a.account_id from cte2 a join Accounts b on a.account_id = b.account_id where total > max_income and t1 > max_income
