2f5952e55c695abcbaf44a7222c553e3af4b452a
Yinan Yang <845280629abc@gmail.com>
2023-04-19 16:49:42 -0500

    Time: 931 ms (89.89%), Space: 14.3 MB (59.70%) - LeetHub

--- 2643-row-with-maximum-ones/2643-row-with-maximum-ones.py
+++ 2643-row-with-maximum-ones/2643-row-with-maximum-ones.py
@@ -3,10 +3,7 @@ class Solution:
         res = [0, 0]
         
         for i in range(len(mat)):
-            count = 0
-            for j in range(len(mat[0])):
-                if mat[i][j] == 1:
-                    count += 1
+            count = sum(mat[i])
             if count > res[1]:
                 res = [i, count]
         
