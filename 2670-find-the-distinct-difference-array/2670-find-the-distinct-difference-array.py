9a0734b2ed8e08df2be1c90ce40953d0353bdb50
Yinan Yang <845280629abc@gmail.com>
2023-05-08 23:23:08 -0500

    Time: 121 ms (75.17%), Space: 16.3 MB (85.32%) - LeetHub

--- 2670-find-the-distinct-difference-array/2670-find-the-distinct-difference-array.py
+++ 2670-find-the-distinct-difference-array/2670-find-the-distinct-difference-array.py
@@ -15,23 +15,16 @@ class Solution:
                 else:
                     pre.append(pre[-1]+1)
                     seen.add(n)
-        back = [0]
-        seen = set()
-        for i in range(len(nums)-1, 0, -1):
-            if len(back) == 1:
-                back.append(1)
-                seen.add(nums[i])
-            else:
-                if nums[i] in seen:
-                    back.append(back[-1])
-                else:
-                    back.append(back[-1]+1)
-                    seen.add(nums[i])
         res = []
-        
-        for i in range(len(pre)):
-            res.append(pre[i]-back[-i-1])
-        return res
+        current = 0
+        seen = set()
+        n = len(nums)
+        for i in range(len(nums)-1, -1, -1):
+            res.append(pre[i]-current)
+            if nums[i] not in seen:
+                current += 1
+                seen.add(nums[i])
+        return reversed(res)
                 
             
         
