139daee11d3740d0b372aae080c1fd027155186a
Yinan Yang <845280629abc@gmail.com>
2023-05-17 23:15:01 -0500

    Time: 907 ms (83.08%), Space: 0B (100.00%) - LeetHub

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -1,6 +1,9 @@
 # Write your MySQL query statement below
 
-with cte as(select *, min(event_date) over(partition by player_id) fd from Activity)
+with cte as(select count(distinct player_id) total from Activity),
+
+cte2 as(select *, min(event_date) over(partition by player_id) gg from Activity)
+
+select ifnull(round(count(a.player_id) / b.total, 2), 0.00) fraction from cte2 a, cte b where a.event_date = DATE_ADD(a.gg, INTERVAL 1 DAY)
 
 
-select round(ct/total, 2) fraction from (select count(distinct player_id) ct from cte where event_date = DATE_ADD(fd, INTERVAL 1 DAY)) a, (select count(distinct player_id) total from Activity) b
