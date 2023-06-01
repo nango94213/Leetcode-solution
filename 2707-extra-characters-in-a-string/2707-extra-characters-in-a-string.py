0737a3c254b9cdbe0ada5f53ae70b8d007705c73
Yinan Yang <845280629abc@gmail.com>
2023-06-01 17:07:45 -0500

    Time: 259 ms (84.80%), Space: 16.4 MB (82.13%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.py
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.py
@@ -1,6 +1,6 @@
 class Solution:
     def minExtraChar(self, s: str, dictionary: List[str]) -> int:
-        dp = [i+1 for i in range(len(s))]
+        dp = [0] * len(s)
         
         dictionary = set(dictionary)
         
@@ -8,10 +8,11 @@ class Solution:
             if s[:i+1] in dictionary:
                 dp[i] = 0
             else:
+                dp[i] = dp[i-1]+1 if i != 0 else 1
                 for j in range(i):
                     if s[j+1:i+1] in dictionary:
                         dp[i] = min(dp[i], dp[j])
-                    dp[i] = min(dp[i], dp[i-1]+1)
+                    
       
         return dp[-1]
                 
