95f78abad310eb26088588275536e4d04a0811a5
Yinan Yang <845280629abc@gmail.com>
2023-06-11 23:32:57 -0500

    Time: 127 ms (36.54%), Space: 58.2 MB (6.11%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cs
+++ 0200-number-of-islands/0200-number-of-islands.cs
@@ -5,6 +5,18 @@ public class Solution {
         m = grid.Length;
         n = grid[0].Length;
         int res = 0;
+        void Dfs(char[][] grid, int x, int y) {
+            grid[x][y] = '0';
+            Tuple<int, int>[] directions = {Tuple.Create(-1, 0), Tuple.Create(1, 0), Tuple.Create(0, -1), Tuple.Create(0, 1)};
+            foreach (var d in directions) {
+               var newX = x + d.Item1;
+               var newY = y + d.Item2;
+               if (0 <= newX && newX < m && 0 <= newY && newY < n && grid[newX][newY] == '1') {
+                   Dfs(grid, newX, newY);
+               }
+            }
+        }
+    
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == '1') {
@@ -15,16 +27,6 @@ public class Solution {
         }
         return res;
     }
-    
-    private void Dfs(char[][] grid, int x, int y) {
-        grid[x][y] = '0';
-        Tuple<int, int>[] directions = {Tuple.Create(-1, 0), Tuple.Create(1, 0), Tuple.Create(0, -1), Tuple.Create(0, 1)};
-        foreach (var d in directions) {
-            var newX = x + d.Item1;
-            var newY = y + d.Item2;
-            if (0 <= newX && newX < m && 0 <= newY && newY < n && grid[newX][newY] == '1') {
-                Dfs(grid, newX, newY);
-            }
-        }
-    }
 }
+    
+    
