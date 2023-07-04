ee622206442c13cde06e6c98c7c5d595a7267aa7
Yinan Yang <845280629abc@gmail.com>
2023-07-03 22:14:22 -0600

    Time: 661 ms (99.72%), Space: 26.8 MB (81.63%) - LeetHub

---
+++ 2762-continuous-subarrays/2762-continuous-subarrays.py
@@ -0,0 +1,28 @@
+class Solution:
+    def continuousSubarrays(self, nums: List[int]) -> int:
+        left = 0
+        res = 0
+        dic = {}
+        for right in range(len(nums)):
+            if nums[right] in dic:
+                dic[nums[right]] = right
+            else:
+                dic[nums[right]] = right
+                d = []
+                for k in dic:
+                    if nums[right] - k > 2:
+                        left = max(left, dic[k]+1)
+                        d.append(k)
+                
+                for k in dic:
+                    if k - nums[right] > 2:
+                        left = max(left, dic[k]+1)
+                        d.append(k)
+                for k in d:
+                    del dic[k]
+            
+            res += right - left + 1
+        
+        return res
+            
+        
