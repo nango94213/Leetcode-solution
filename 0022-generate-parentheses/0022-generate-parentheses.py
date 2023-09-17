e16b720854492000b82d92b72b34be2a623dc42c
Yinan Yang <845280629abc@gmail.com>
2023-09-16 20:05:54 -0600

    Time: 33 ms (94.82%), Space: 16.6 MB (85.95%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,16 +1,16 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         res = []
-        
         def dfs(left, right, path):
-            if left > right or left < 0 or right < 0:
+            if left < 0 or right < 0:
                 return
-            
+            if left > right:
+                return
             if left == 0 and right == 0:
                 res.append(path)
             
             dfs(left-1, right, path+'(')
             dfs(left, right-1, path+')')
+        
         dfs(n, n, '')
         return res
-        
