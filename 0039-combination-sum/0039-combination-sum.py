ac9b86af473d8f33cf5901ffc4d23c77096033b2
Yinan Yang <845280629abc@gmail.com>
2023-09-17 00:37:11 -0600

    Time: 54 ms (86.01%), Space: 16.3 MB (96.17%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,16 +1,15 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-
-        res = []      
-        def dfs(pool, path, current):
-            if current > target:
-                return 
-            if current == target:
+        res = []
+        def dfs(pool, path, total):
+            
+            if total > target:
+                return
+            if total == target:
                 res.append(path)
             
             for i in range(len(pool)):
-     
-                dfs(pool[i:], path+[pool[i]], current+pool[i])
+                dfs(pool[i:], path+[pool[i]], total+pool[i])
         
         dfs(candidates, [], 0)
         
