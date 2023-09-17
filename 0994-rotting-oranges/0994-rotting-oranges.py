0c178694f6c53ce6cb79b88e70356bd7f0645928
Yinan Yang <845280629abc@gmail.com>
2023-09-17 12:24:40 -0600

    Time: 66 ms (20.17%), Space: 16.3 MB (85.20%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,38 +1,34 @@
 import collections
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        directions = [(0, -1), (-1, 0), (0, 1), (1, 0)]
-        
+        q = collections.deque()
+        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
         m = len(grid)
         n = len(grid[0])
-        
-        q = collections.deque()
-        
-        good = 0
-        
+        count = 0
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 2:
                     q.append((i, j))
-                
                 if grid[i][j] == 1:
-                    good += 1
-        if good == 0:
-            return 0
-        time = 0
+                    count += 1
+                    
+
+        time = -1
+        
         while q:
             for _ in range(len(q)):
                 current = q.popleft()
                 
                 for d in directions:
-                    i = current[0] + d[0]
-                    j = current[1] + d[1]
+                    x = current[0] + d[0]
+                    y = current[1] + d[1]
                     
-                    if 0 <= i < m and 0 <= j < n and grid[i][j] == 1:
-                        good -= 1
-                        grid[i][j] = 2
-                        q.append((i, j))
-            time += 1
-        
-        return time-1 if good == 0 else -1
+                    if 0 <= x < m and 0 <= y < n and grid[x][y] == 1:
+                        grid[x][y] = 2
+                        count -= 1
+                        q.append((x, y))
             
+            time += 1
+
+        return max(time, 0) if count == 0 else -1
