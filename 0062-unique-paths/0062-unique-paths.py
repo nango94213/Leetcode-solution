2589a94a25dd116fe968ce00e09e82a2ab6cc020
Yinan Yang <845280629abc@gmail.com>
2023-09-16 23:59:09 -0600

    Time: 42 ms (36.94%), Space: 16.2 MB (94.35%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,8 +1,6 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        
-        
-        dp = [[0]*n for _ in range(m)]
+        dp = [[0] * n for _ in range(m)]
         
         for i in range(m):
             for j in range(n):
@@ -10,5 +8,6 @@ class Solution:
                     dp[i][j] = 1
                 else:
                     dp[i][j] = dp[i][j-1] + dp[i-1][j]
-              
+        
         return dp[-1][-1]
+        
