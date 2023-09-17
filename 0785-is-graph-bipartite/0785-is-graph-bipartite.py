588d93c4b372231f9868f83b0491d98ecd816917
Yinan Yang <845280629abc@gmail.com>
2023-09-17 17:47:35 -0600

    Time: 161 ms (67.91%), Space: 16.6 MB (99.25%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.py
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.py
@@ -1,22 +1,23 @@
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
-        
         dic = {}
         
         for i in range(len(graph)):
             if i not in dic:
                 dic[i] = True
+                
                 q = collections.deque([(i, True)])
                 
                 while q:
                     current, group = q.popleft()
                     
-                    for o in graph[current]:
-                        if o in dic and dic[o] == group:
+                    for n in graph[current]:
+                        if n in dic and dic[n] == group:
                             return False
-                        if o not in dic:
-                            dic[o] = not group
-                            q.append((o, not group))
+                        if n not in dic:
+                            dic[n] = not group
+                            q.append((n, not group))
+        
+        
         return True
-                
         
