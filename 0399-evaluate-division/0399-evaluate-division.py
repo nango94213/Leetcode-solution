159db12c28cef23110435c4eb37c84818be80145
Yinan Yang <845280629abc@gmail.com>
2023-09-18 20:37:35 -0600

    Time: 42 ms (46.28%), Space: 16.4 MB (68.84%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.py
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -1,33 +1,41 @@
-import collections
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        
+        
         graph = collections.defaultdict(defaultdict)
         
-        for i in range(len(equations)):
-            graph[equations[i][0]][equations[i][1]] = values[i]
-            graph[equations[i][1]][equations[i][0]] = 1 / values[i]
+        for i, e in enumerate(equations):
+            graph[e[0]][e[1]] = values[i]
+            graph[e[1]][e[0]] = 1.0 / values[i]
         
-        def check(s, e):
-            q = collections.deque([(s, 1.0)])
-            seen = set([s])
+        def check(start, end):
+            
+            q = collections.deque([(1.0, start)])
+            seen = set([start])
+            
             while q:
-                current, value = q.popleft()
-                if current == e:
-                    return value
-                for o in graph[current]:
+                current = q.popleft()
+                
+                if current[1] == end:
+                    return current[0]
+                
+                for o in graph[current[1]]:
                     if o not in seen:
                         seen.add(o)
-                        q.append((o, graph[current][o]*value))
+                        q.append((current[0]*graph[current[1]][o], o))
+            
             
             return -1.0
+        
         res = []
+        
         for s, e in queries:
             if s not in graph or e not in graph:
                 res.append(-1.0)
+            
             elif s == e:
                 res.append(1.0)
             else:
                 res.append(check(s, e))
         
         return res
-        
