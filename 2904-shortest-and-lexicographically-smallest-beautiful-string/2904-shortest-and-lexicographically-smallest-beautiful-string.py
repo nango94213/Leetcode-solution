772d8b8afdc2bb51b52b78654821ac7667417c62
Yinan Yang <845280629abc@gmail.com>
2023-10-19 21:49:22 -0600

    Time: 45 ms (65.43%), Space: 16.3 MB (58.85%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.py
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.py
@@ -1,7 +1,7 @@
 class Solution:
     def shortestBeautifulSubstring(self, s: str, k: int) -> str:
         g = k
-        res = float('inf')
+        res = s
         left = 0
         marker = False
         for right in range(len(s)):
@@ -10,31 +10,16 @@ class Solution:
             
             while k == 0:
                 marker = True
-                res = min(res, right-left+1)
+
+                if len(res) > right - left + 1:
+                        res = s[left:right+1]
+                elif len(res) == right - left + 1:
+                        res = min(res, s[left:right+1])
+
                 if s[left] == '1':
                     k += 1
                 left += 1
-            
-        
-        lowest = s
-        left = 0
 
-        for right in range(len(s)):
-            if s[right] == '1':
-                g -= 1
-            
-            while g == 0:
-                
-                if right-left+1 == res:
-   
-                    if len(lowest) > right - left + 1:
-                        lowest = s[left:right+1]
-                    else:
-                        lowest = min(lowest, s[left:right+1])
-                    
-                if s[left] == '1':
-                    g += 1
-                left += 1
-        return lowest if marker else ""
+        return res if marker else ""
         
         
