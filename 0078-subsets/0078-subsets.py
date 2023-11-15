eefa305b4b27043e2c4e6b1716dfdcd034f52d3c
Yinan Yang <845280629abc@gmail.com>
2023-11-15 12:42:20 -0700

    Time: 40 ms (59.68%), Space: 16.6 MB (13.74%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,15 +1,14 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        
         res = []
-        
+        n = len(nums)
         def dfs(pool, path):
+
             res.append(path)
             
             for i in range(len(pool)):
                 dfs(pool[i+1:], path+[pool[i]])
         
         dfs(nums, [])
-        
         return res
         
