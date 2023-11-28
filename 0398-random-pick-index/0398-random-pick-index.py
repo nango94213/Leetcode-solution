513bea1c0991a274aee30d25215c8183af6d60bd
Yinan Yang <845280629abc@gmail.com>
2023-11-27 17:29:38 -0700

    Time: 315 ms (65.16%), Space: 26 MB (76.47%) - LeetHub

--- 0398-random-pick-index/0398-random-pick-index.py
+++ 0398-random-pick-index/0398-random-pick-index.py
@@ -1,17 +1,12 @@
-import collections
-import random
 class Solution:
 
     def __init__(self, nums: List[int]):
         self.dic = collections.defaultdict(list)
-        
         for i, v in enumerate(nums):
             self.dic[v].append(i)
-            
+
     def pick(self, target: int) -> int:
-        
         return random.choice(self.dic[target])
-        
 
 
 # Your Solution object will be instantiated and called as such:
