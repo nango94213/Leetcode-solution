ed56f336c25ad40fda0e7b24020f229a793c4893
Yinan Yang <845280629abc@gmail.com>
2023-11-30 20:35:03 -0700

    Time: 45 ms (14.39%), Space: 16.3 MB (24.35%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -1,15 +1,14 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         res = []
-        m = len(matrix)
-        n = len(matrix[0])
         
         left = 0
-        right = n - 1
+        right = len(matrix[0]) - 1
         top = 0
-        bottom = m - 1
+        bottom = len(matrix) - 1
+        total = len(matrix) * len(matrix[0])
         
-        while True:
+        while len(res) < total:
             for i in range(left, right+1):
                 res.append(matrix[top][i])
             
@@ -23,12 +22,9 @@ class Solution:
             if left != right:
                 for i in range(bottom-1, top, -1):
                     res.append(matrix[i][left])
-            if len(res) == m*n:
-                break
             left += 1
             right -= 1
             top += 1
             bottom -= 1
         
         return res
-        
