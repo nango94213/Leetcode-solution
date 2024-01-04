bdd314c73dc2fdc92e159463f3a82fe8bb506053
Yinan Yang <845280629abc@gmail.com>
2024-01-04 00:34:59 -0600

    Time: 74 ms (89.97%), Space: 17.2 MB (15.84%) - LeetHub

--- 2848-points-that-intersect-with-cars/2848-points-that-intersect-with-cars.py
+++ 2848-points-that-intersect-with-cars/2848-points-that-intersect-with-cars.py
@@ -7,13 +7,13 @@ class Solution:
             dic[n[0]] += 1
             dic[n[1]+1] -= 1
         current = 0 
+        total = 0
         for i in range(1, len(dic)):
             current += dic[i]
             if current > 0:
-                dic[i] = 1
-            else:
-                dic[i] = 0
+                total += 1
+
       
-        return sum(dic)
+        return total
         
         
